534947c3c4dc7d8347453249d2fd7d1f54baa74f
Walter Bender <walter.bender@gmail.com>
2016-02-17 18:26:30 -0500

    revert octave shift for A and B in lilypond... the problem is elsewhere

--- js/logo.js
+++ js/logo.js
@@ -4079,7 +4079,7 @@ function Logo(matrix, canvas, blocks, turtles, stage,
             // , and ' for shifts in octave.
             // Also, notes must be lowercase.
             // And the octave bounday is at C, not A.
-            if (note[0] === 'A' || note[0] === 'B') {
+            if (false) { // (note[0] === 'A' || note[0] === 'B') {
 		return note.replace(/♯/g, 'is').replace(/♭/g, 'es').replace(/10/g, "'''''''").replace(/1/g, ',,,').replace(/2/g, ',,').replace(/3/g, ',').replace(/4/g, "").replace(/5/g, "'").replace(/6/g, "''").replace(/7/g, "'''").replace(/8/g, "'''''").replace(/9/g, "''''''").toLowerCase();
             } else {
 		return note.replace(/♯/g, 'is').replace(/♭/g, 'es').replace(/10/g, "''''''''").replace(/1/g, ',,').replace(/2/g, ',').replace(/3/g, '').replace(/4/g, "'").replace(/5/g, "''").replace(/6/g, "'''").replace(/7/g, "''''").replace(/8/g, "''''''").replace(/9/g, "'''''''").toLowerCase();
