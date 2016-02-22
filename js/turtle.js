fe622a95f7f91f967c6e1cb923f3d7bd1073ae62
Walter Bender <walter.bender@gmail.com>
2016-02-21 22:20:17 -0500

    add missing function

--- js/turtle.js
+++ js/turtle.js
@@ -1014,3 +1014,13 @@ function makeTurtleBitmap(me, data, name, callback, extras) {
     };
     img.src = 'data:image/svg+xml;base64,' + window.btoa(unescape(encodeURIComponent(data)));
 };
+
+
+function hex2rgb(hex) {
+    var bigint = parseInt(hex, 16);
+    var r = (bigint >> 16) & 255;
+    var g = (bigint >> 8) & 255;
+    var b = bigint & 255;
+
+    return "rgba(" + r + "," + g + "," + b + ",1)";
+}
