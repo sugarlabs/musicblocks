49228fda884b98ac82740ec71bce6578baf961a0
Walter Bender <walter.bender@gmail.com>
2016-03-02 13:03:31 -0500

    clean up

--- js/protoblocks.js
+++ js/protoblocks.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2014,2015 Walter Bender
+// Copyright (c) 2014-16 Walter Bender
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the The GNU Affero General Public
