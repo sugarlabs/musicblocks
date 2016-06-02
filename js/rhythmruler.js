676c7046af193e8b44019f2dd3a6a3b8d294dbf6
jordan <hemantkasat@gmail.com>
2016-06-02 22:18:48 +0530

    replaced function

--- js/rhythmruler.js
+++ js/rhythmruler.js
@@ -2,9 +2,42 @@ const RHYTHMRULERHEIGHT = 100;
 
 function RhythmRuler () {
 
+
+
+  function dissectRuler (event) {
+        var inputNum = prompt("Divide By:" , 2  );
+        var cell = event.target;
+        var newCellIndex = cell.cellIndex;
+        var newCellWidth = Math.floor(parseFloat(cell.style.width)/inputNum) + 'px';
+        var ruler = document.getElementById('ruler');
+        ruler.deleteCell(newCellIndex);
+        var newCellHeight = cell.style.height;
+
+        for ( var i = 0; i < inputNum; i++) {
+            var newCell = ruler.insertCell(newCellIndex+i);
+            newCell.style.width = newCellWidth;
+            newCell.style.height = newCellHeight;
+            newCell.style.minWidth = newCell.style.width;
+            newCell.style.maxWidth = newCell.style.width;
+            newCell.style.backgroundColor = MATRIXLABELCOLOR;
+            
+            newCell.addEventListener("click", function(event) {
+              dissectRuler(event);
+            });
+            
+            newCell.onmouseover=function() {
+              this.style.backgroundColor = MATRIXBUTTONCOLORHOVER;
+            }    
+            
+            newCell.onmouseout=function() {
+              this.style.backgroundColor = MATRIXLABELCOLOR;
+            }  
+        }
+  }
 	this.init = function(logo) {
 		    console.log("init RhythmRuler");
 		    this.logo = logo;
+        docById('rulerbody').style.display = 'inline';
         console.log('setting RhythmRuler visible');
         docById('rulerbody').style.visibility = 'visible';
         docById('rulerbody').style.border = 2;
@@ -22,8 +55,7 @@ function RhythmRuler () {
 
         var x = document.createElement('TABLE');
         x.setAttribute('id', 'myTable');
-        x.setAttribute('table-layout', 'fixed');
-        x.setAttribute('display', 'table');
+      //  x.setAttribute('table-layout', 'fixed');
         x.style.textAlign = 'center';
 
         var rulerbodyDiv = docById('rulerbody');
@@ -33,7 +65,7 @@ function RhythmRuler () {
         rulerbodyDivPosition = rulerbodyDiv.getBoundingClientRect();
 
         var table = docById('myTable');
-        table.style.display = 'block';
+      //  table.style.display = 'block';
         var header = table.createTHead();
         var row = header.insertRow(0);
         row.style.left = Math.floor(rulerbodyDivPosition.left) + 'px';
@@ -42,12 +74,31 @@ function RhythmRuler () {
         var iconSize = Math.floor(this.cellScale * 24);
 
         var cell = row.insertCell(-1);
+        cell.innerHTML = '&nbsp;&nbsp;<img src="header-icons/export-chunk.svg" title="' + _('save') + '" alt="' + _('save') + '" height="' + iconSize + '" width="' + iconSize + '" vertical-align="middle">&nbsp;&nbsp;';
+        cell.style.width = Math.floor(MATRIXBUTTONHEIGHT * this.cellScale) + 'px';
+        cell.style.minWidth = cell.style.width;
+        cell.style.maxWidth = cell.style.width;
+        cell.style.height = Math.floor(MATRIXBUTTONHEIGHT * this.cellScale) + 'px';
+        cell.style.backgroundColor = MATRIXBUTTONCOLOR;
+       // cell.style.display = 'inline-block';
+        cell.onclick=function() {
+            
+        }
+        cell.onmouseover=function() {
+            this.style.backgroundColor = MATRIXBUTTONCOLORHOVER;
+        }
+        cell.onmouseout=function() {
+            this.style.backgroundColor = MATRIXBUTTONCOLOR;
+        }
+
+        var cell = row.insertCell(1);
         cell.innerHTML = '&nbsp;&nbsp;<img src="header-icons/close-button.svg" title="' + _('close') + '" alt="' + _('close') + '" height="' + iconSize + '" width="' + iconSize + '" vertical-align="middle">&nbsp;&nbsp;';
         cell.style.width = Math.floor(MATRIXBUTTONHEIGHT * this.cellScale) + 'px';
         cell.style.minWidth = cell.style.width;
         cell.style.maxWidth = cell.style.width;
         cell.style.height = Math.floor(MATRIXBUTTONHEIGHT * this.cellScale) + 'px';
         cell.style.backgroundColor = MATRIXBUTTONCOLOR;
+     //   cell.style.display = 'inline-block';
         cell.onclick=function() {
             docById('rulerbody').style.visibility = 'hidden';
             docById('rulerbody').style.border = 0;
@@ -69,8 +120,20 @@ function RhythmRuler () {
         cell.style.minWidth = cell.style.width;
         cell.style.maxWidth = cell.style.width;
         cell.style.height = Math.floor(RHYTHMRULERHEIGHT * this.cellScale) + 'px';          
-        cell.style.backgroundColor = MATRIXBUTTONCOLOR;
-        cell.setAttribute('onclick', 'dissectRuler(event)');      
+        cell.style.backgroundColor = MATRIXLABELCOLOR;
+
+        
+
+        cell.addEventListener("click", function(event) {
+          dissectRuler(event);
+        });
+
+        cell.onmouseover=function() {
+          this.style.backgroundColor = MATRIXBUTTONCOLORHOVER;
+        }    
+        cell.onmouseout=function() {
+          this.style.backgroundColor = MATRIXLABELCOLOR;
+        }
 
 	}
 
