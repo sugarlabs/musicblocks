1c17b1d90b2d3413610f99d9aed76b83ed676841
jordan <hemantkasat@gmail.com>
2016-06-02 08:08:42 +0530

    added dissection

--- js/rhythmruler.js
+++ js/rhythmruler.js
@@ -1,12 +1,18 @@
+const RHYTHMRULERHEIGHT = 100;
+
 function RhythmRuler () {
 
 	this.init = function(logo) {
-		console.log("init RhythmRuler");
-		this.logo = logo;
-		docById('rulerbody').style.display = 'inline';
+		    console.log("init RhythmRuler");
+		    this.logo = logo;
         console.log('setting RhythmRuler visible');
         docById('rulerbody').style.visibility = 'visible';
         docById('rulerbody').style.border = 2;
+
+        var w = window.innerWidth;
+        this.cellScale = w / 1200;
+        docById('rulerbody').style.width = Math.floor(w / 2) + 'px';
+        docById('rulerbody').style.overflowX = 'auto';
 
         var table = docById('myTable');
 
@@ -16,6 +22,8 @@ function RhythmRuler () {
 
         var x = document.createElement('TABLE');
         x.setAttribute('id', 'myTable');
+        x.setAttribute('table-layout', 'fixed');
+        x.setAttribute('display', 'table');
         x.style.textAlign = 'center';
 
         var rulerbodyDiv = docById('rulerbody');
@@ -25,25 +33,23 @@ function RhythmRuler () {
         rulerbodyDivPosition = rulerbodyDiv.getBoundingClientRect();
 
         var table = docById('myTable');
+        table.style.display = 'block';
         var header = table.createTHead();
         var row = header.insertRow(0);
         row.style.left = Math.floor(rulerbodyDivPosition.left) + 'px';
         row.style.top = Math.floor(rulerbodyDivPosition.top) + 'px';
 
-        var iconSize = 20 + 'px';
-
+        var iconSize = Math.floor(this.cellScale * 24);
 
         var cell = row.insertCell(-1);
         cell.innerHTML = '&nbsp;&nbsp;<img src="header-icons/close-button.svg" title="' + _('close') + '" alt="' + _('close') + '" height="' + iconSize + '" width="' + iconSize + '" vertical-align="middle">&nbsp;&nbsp;';
-        cell.style.width = 40 + 'px';
+        cell.style.width = Math.floor(MATRIXBUTTONHEIGHT * this.cellScale) + 'px';
         cell.style.minWidth = cell.style.width;
         cell.style.maxWidth = cell.style.width;
-        cell.style.height = 10 + 'px';
+        cell.style.height = Math.floor(MATRIXBUTTONHEIGHT * this.cellScale) + 'px';
         cell.style.backgroundColor = MATRIXBUTTONCOLOR;
         cell.onclick=function() {
             docById('rulerbody').style.visibility = 'hidden';
-            document.getElementsByClassName('hRule')[0].style.visibility = 'hidden';
-            document.getElementsByClassName('mousePosBox')[0].style.visibility = 'hidden';
             docById('rulerbody').style.border = 0;
         }
         cell.onmouseover=function() {
@@ -52,6 +58,19 @@ function RhythmRuler () {
         cell.onmouseout=function() {
             this.style.backgroundColor = MATRIXBUTTONCOLOR;
         }
+
+        var row = header.insertRow(1);
+        row.style.left = Math.floor(rulerbodyDivPosition.left) + 'px';
+        row.style.top = Math.floor(MATRIXBUTTONHEIGHT * this.cellScale) + 'px';
+        row.setAttribute('id', 'ruler');
+
+        var cell = row.insertCell(-1);
+        cell.style.width = Math.floor(rulerbodyDivPosition.width) + 'px';
+        cell.style.minWidth = cell.style.width;
+        cell.style.maxWidth = cell.style.width;
+        cell.style.height = Math.floor(RHYTHMRULERHEIGHT * this.cellScale) + 'px';          
+        cell.style.backgroundColor = MATRIXBUTTONCOLOR;
+        cell.setAttribute('onclick', 'dissectRuler(event)');      
 
 	}
 
