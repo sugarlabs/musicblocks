b65b5f5489abaa3f0a1c6b64ead257edd89f4368
Walter Bender <walter.bender@gmail.com>
2016-07-17 16:27:19 -0400

    just say no to tabs

--- js/modewidget.js
+++ js/modewidget.js
@@ -555,11 +555,11 @@ function ModeWidget() {
                 } else {
                     // FIXME: better way to do  comparision?
                     if (cell.style.backgroundColor === "rgb(195, 116, 233)") { // MATRIXBUTTONCOLOR) {
-			cell.style.backgroundColor = MATRIXBUTTONCOLORHOVER;
-		    } else {
-			cell.style.backgroundColor = MATRIXBUTTONCOLOR;
-		    }
-		}
+                        cell.style.backgroundColor = MATRIXBUTTONCOLORHOVER;
+                    } else {
+                        cell.style.backgroundColor = MATRIXBUTTONCOLOR;
+                    }
+                }
             }
 
             var noteToPlay = that._logo.getNote(that._pitch, 4, that.cells[i]);
