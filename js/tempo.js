2667f33326f1ebcd06739c3a674c152919f5ad54
jordan <hemantkasat@gmail.com>
2016-07-27 11:40:43 +0530

    Speed Up and Speed Down enabled

--- js/tempo.js
+++ js/tempo.js
@@ -81,10 +81,52 @@ function Tempo () {
 	this.draw = function () { 
 		var that = this;
 		var canvasRect = canvas.getBoundingClientRect();
-  		ctx.clearRect(canvasRect.left,canvasRect.top, WIDTH, HEIGHT);
+  		ctx.clearRect(0,0,canvas.width,canvas.height);
   		console.log(x);
-  		this.circle(x+10,y+20,30); 
-  		x += 1;
+  	//	that.circle(x+10,y+20,30); 
+  		ctx.beginPath(); 
+		ctx.fillStyle = ""; 
+  		ctx.arc(x, y, 30, 0, Math.PI*2); 
+		ctx.fill(); 
+		ctx.closePath();
+		if (x + mx > canvas.width || x + mx < 0) {
+	
+	if(x+mx>canvas.width)
+		tempmx=-1;
+			
+	else
+		tempmx=1;
+		
+    mx = -mx;
+//sound.play(); 
+var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+
+
+}
+ctx.fillStyle = color; 
+var colorC = '#';
+    for (var i = 0; i < 6; i++ ) {
+        colorC += letters[Math.floor(Math.random() * 16)];
+
+}
+document.getElementById("TempoCanvas").style.background = colorC;
+document.getElementById("TempoDiv").style.background = colorC;
+
+}
+  if (y+ my > canvas.height || y+ my < 0) 
+
+    my = -my; 
+
+  x += mx; 
+
+
+ // y += my; 
+
+
+
 	};
 
 	this.init = function () {
@@ -96,10 +138,10 @@ function Tempo () {
         var w = window.innerWidth;
         docById('TempoDiv').style.width = Math.floor(w / 2) + 'px';
         docById('TempoCanvas').style.width = Math.floor(w / 2) + 'px';
-        docById('TempoCanvas').style.height = Math.floor(w / 2) + 'px';
+        docById('TempoCanvas').style.height = Math.floor(w / 4) + 'px';
 
         WIDTH = Math.floor(w / 2) + 'px';
-        HEIGHT = Math.floor(w / 2) + 'px';
+        HEIGHT = Math.floor(w / 4) + 'px';
  
         var TempoDiv = docById('TempoDiv');
 
@@ -108,11 +150,27 @@ function Tempo () {
 		console.log(ctx);
 		var canvasRect = canvas.getBoundingClientRect();
 
-		x = canvasRect.left;
-		y = canvasRect.top;
+		x = canvas.width;
+		y = 100;
+		console.log(x);
+		console.log(y);
 		console.log(canvas.getBoundingClientRect());
-		thisTempo.draw();
-  		setInterval(thisTempo.draw(), 5);
+		setInterval(thisTempo.draw,5);
+
+		var SpeedUpButton = document.createElement("BUTTON");
+		SpeedUpButton.setAttribute('id','up');
+		SpeedUpButton.textContent = "Speed Up";
+		SpeedUpButton.onclick = thisTempo.speedUp;
+		SpeedUpButton.className = 'black';
+		TempoDiv.appendChild(SpeedUpButton); 
+
+		var SpeedDownButton = document.createElement("BUTTON");
+		SpeedDownButton.setAttribute('id', 'down');
+		SpeedDownButton.textContent = "Speed Down";
+		SpeedDownButton.onclick = thisTempo.speedDown;
+		SpeedDownButton.className = 'black';
+		TempoDiv.appendChild(SpeedDownButton);
+//		ctx.clearRect(0,0,canvas.width,canvas.height);
 	};
 
 };
