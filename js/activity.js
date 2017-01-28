8b89b3150cc0f6d0a4518008d2208cd7b2e3d377
Tymon Radzik <dwgipk@gmail.com>
2017-01-28 23:57:54 +0100

    Fixed #323; removed never used piece of code
    Standard debugging and code look-up shows the function is never called during normal usage. To be removed.

--- js/activity.js
+++ js/activity.js
@@ -1468,11 +1468,6 @@ define(function (require) {
             }
         };
 
-        function stop() {
-            // FIXME: who calls this???
-            createjs.Ticker.removeEventListener('tick', __tick);
-        };
-
         function onStopTurtle() {
             // TODO: plugin support
             if (stopTurtleContainer.visible) {
