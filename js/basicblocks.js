678af4b84276db4d60846c13905b6892f07f3e5b
Tabs16 <taybawasim@gmail.com>
2017-02-08 19:32:42 +0530

    Fixes issue #573

--- js/basicblocks.js
+++ js/basicblocks.js
@@ -798,7 +798,7 @@ function initBasicProtoBlocks(palettes, blocks) {
     var articulationBlock = new ProtoBlock('articulation');
     articulationBlock.palette = palettes.dict['tone'];
     blocks.protoBlockDict['articulation'] = articulationBlock;
-    articulationBlock.staticLabels.push(_('articulation'));
+    articulationBlock.staticLabels.push(_('set relative voice volume'));
     articulationBlock.adjustWidthToLabel();
     articulationBlock.flowClampOneArgBlock();
     articulationBlock.defaults.push(25);
