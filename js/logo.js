dcba392dc0fa42c21625db52638a51987cd491bc
Walter Bender <walter.bender@gmail.com>
2017-09-01 09:25:53 -0400

    support polyphonic rhythms in the rhythmruler

--- js/logo.js
+++ js/logo.js
@@ -3646,13 +3646,15 @@ function Logo () {
                     that._processNote(noteBeatValue, blk, turtle);
                 }
             } else if (that.inRhythmRuler) {
-                // Temporary work-around to #272.
-                if (that.rhythmRulerMeasure === null) {
-                    that.rhythmRulerMeasure = args[0] * args[1];
-                } else if (that.rhythmRulerMeasure != (args[0] * args[1])) {
-                    that.errorMsg('Rhythm Ruler imbalance.', blk);
-                    that.stopTurtle = true;
-                }
+                // We don't check for balance since we want to support
+                // polyphonic rhythms.
+                // remove temporary work-around to #272.
+                // if (that.rhythmRulerMeasure === null) {
+                //     that.rhythmRulerMeasure = args[0] * args[1];
+                // } else if (that.rhythmRulerMeasure != (args[0] * args[1])) {
+                //     that.errorMsg('Rhythm Ruler imbalance.', blk);
+                //     that.stopTurtle = true;
+                // }
 
                 // Since there maybe more than one instance of the
                 // same drum, e.g., if a repeat is used, we look from
