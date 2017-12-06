a8dc51aee1359ee715ec4497319af47df68deed2
pipix51 <koh.pirong@dhs.sg>
2017-12-06 22:44:31 +0800

    Update basicblocks.js

--- js/basicblocks.js
+++ js/basicblocks.js
@@ -1130,15 +1130,6 @@ function initBasicProtoBlocks(palettes, blocks) {
     duoSynthBlock.twoArgBlock();
     duoSynthBlock.dockTypes[1] = 'numberin';
     duoSynthBlock.dockTypes[2] = 'numberin';
-
-    var noise = new ProtoBlock('noise');
-    noise.palette = palettes.dict['tone'];
-    blocks.protoBlockDict['noise'] = noise;
-    noise.staticLabels.push(_('noise'));
-    noise.adjustWidthToLabel();
-    noise.defaults.push(_('white'));
-    noise.oneArgBlock();
-    noise.dockTypes[1] = 'textin';
     
     var distortionBlock = new ProtoBlock('dis');
     distortionBlock.palette = palettes.dict['tone'];
