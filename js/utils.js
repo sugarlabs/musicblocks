c22f3caebac261664e52826a74971ac1418cb544
Walter Bender <walter.bender@gmail.com>
2017-12-08 12:35:51 -0500

    add missing var

--- js/utils.js
+++ js/utils.js
@@ -212,8 +212,8 @@ function fileBasename (file) {
 // Needed to generate new data for localization.ini
 // var translated = "";
 function _ (text) {
-    replaced = text;
-    replace = [",", "(", ")", "?", "¿", "<", ">", ".", '"\n', '"', ":", "%s", "%d", "/", "'", ";", "×", "!", "¡"];
+    var replaced = text;
+    var replace = [",", "(", ")", "?", "¿", "<", ">", ".", '"\n', '"', ":", "%s", "%d", "/", "'", ";", "×", "!", "¡"];
     for (var p = 0; p < replace.length; p++) {
         replaced = replaced.replace(replace[p], "");
     }
@@ -226,7 +226,7 @@ function _ (text) {
     //  }
     // You can log translated in console.log(translated)
     try {
-        translation = document.webL10n.get(replaced);
+        var translation = document.webL10n.get(replaced);
         if (translation === '') {
             translation = text;
         };
