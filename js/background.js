baa2a7b0b9374c03a4f6bfb84976683ad1398e32
Carol Chen <carolyfisher@gmail.com>
2017-12-16 14:46:10 -0500

    Make background.js compatible with firefox

--- js/background.js
+++ js/background.js
@@ -2,7 +2,21 @@ chrome.browserAction.onClicked.addListener(function(tab) {
   window.open(chrome.runtime.getURL("index.html"));
 });
 
-chrome.runtime.onInstalled.addListener(function (callback) {
-  window.open(chrome.runtime.getURL("index.html"));
+// Firefox
+browser.browserAction.onClicked.addListener(function(tab) {
+    browser.tabs.update({
+        url: "index.html"
+    });
+});
 
+
+chrome.runtime.onInstalled.addListener(function (tab) {
+  window.open(chrome.runtime.getURL("index.html"));
+});
+
+//Firefox
+browser.runtime.onInstalled.addListener(function (tab) {
+  browser.tabs.update({
+      url: "index.html"
+  });
 });
