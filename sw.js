5e05de53f07bcdebebbf4ed6dcd09efa5bf050d1
eohomegrownapps <euan.l.y.ong@gmail.com>
2018-01-12 21:15:56 +0000

    Removed new project SVG cache code
    As in title

--- sw.js
+++ sw.js
@@ -25,7 +25,6 @@ var urlsToCache = [
     'header-icons/down.svg',
     'header-icons/edit.svg',
     'header-icons/effects.svg',
-    'header-icons/new-project-button.svg',
     'header-icons/envelope.svg',
     'header-icons/erase-button.svg',
     'header-icons/export-button.svg',
