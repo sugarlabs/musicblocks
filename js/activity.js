a22b145c5314bf7873b2bc36d88e1ff04c0c3453
Walter Bender <walter.bender@gmail.com>
2018-03-03 15:53:27 -0500

    clean up

--- js/activity.js
+++ js/activity.js
@@ -120,21 +120,6 @@ define(MYDEFINES, function (compatibility) {
             console.log(e);
         }
 
-/*
-// Prefer camera resolution nearest to 1280x720.
-var constraints = { audio: true, video: { width: 1280, height: 720 } }; 
-
-navigator.mediaDevices.getUserMedia(constraints)
-.then(function(mediaStream) {
-  var video = document.querySelector('video');
-  video.srcObject = mediaStream;
-  video.onloadedmetadata = function(e) {
-    video.play();
-  };
-})
-.catch(function(err) { console.log(err.name + ": " + err.message); }); // always check for errors at the end.
-*/	    
-
         document.title = TITLESTRING;
 
         var canvas = docById('myCanvas');
