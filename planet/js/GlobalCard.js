7909a7965e7555e2511338f533468f76122b788f
eohomegrownapps <euan.l.y.ong@gmail.com>
2018-03-04 22:48:33 +0000

    Merged DSP / defines commit
    As in title

---
+++ planet/js/GlobalCard.js
@@ -0,0 +1,164 @@
+// Copyright (c) 2017 Euan Ong
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the The GNU Affero General Public
+// License as published by the Free Software Foundation; either
+// version 3 of the License, or (at your option) any later version.
+//
+// You should have received a copy of the GNU Affero General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, 51 Franklin Street, Suite 500 Boston, MA 02110-1335 USA
+
+function GlobalCard(Planet) {
+	this.ProjectData = null;
+	this.id = null;
+	this.PlaceholderImage = "images/planetgraphic.png";
+	this.renderData = '\
+<div class="col no-margin-left s12 m6 l4"> \
+    <div class="card"> \
+        <div class="card-image"> \
+            <img class="project-image project-card-image" id="global-project-image-{ID}" src="images/planetgraphic.png"> \
+        </div> \
+        <div class="card-content"> \
+            <span class="card-title global-title grey-text text-darken-4" id="global-project-title-{ID}"></span> \
+            <div id="global-project-tags-{ID}"> \
+            </div> \
+        </div> \
+        <div class="card-action"> \
+            <div class="flexcontainer"> \
+                <a class="project-icon" id="global-project-more-details-{ID}">'+_('More Details')+'</a> \
+                <a class="project-icon"></a> \
+                <a class="project-icon tooltipped" data-position="bottom" data-delay="50" data-tooltip="'+_('Like project')+'"><i class="material-icons"id="global-like-icon-{ID}"></i><span class="likes-count" id="global-project-likes-{ID}"></span></a> \
+            	<div id="global-share-{ID}"> \
+					<a class="project-icon tooltipped" data-position="bottom" data-delay="50" data-tooltip="'+_('Share project')+'" id="global-project-share-{ID}"><i class="material-icons">share</i></a> \
+					<div class="card share-card" id="global-sharebox-{ID}" style="display:none;"> \
+						<div class="card-content shareurltext"> \
+							<div class="shareurltitle">'+_('Share')+'</div> \
+							<input type="text" name="shareurl" class="shareurlinput" data-originalurl="https://walterbender.github.io/musicblocks/index.html?id={ID}"> \
+							<div class="shareurl-advanced" id="global-advanced-{ID}"> \
+								<div class="shareurltitle">'+_('Flags')+'</div> \
+								<div><input type="checkbox" name="run" id="global-checkboxrun-{ID}" checked><label for="global-checkboxrun-{ID}">'+_('Run project on startup.')+'</label></div> \
+								<div><input type="checkbox" name="show" id="global-checkboxshow-{ID}"><label for="global-checkboxshow-{ID}">'+_('Show code blocks on startup.')+'</label></div> \
+								<div><input type="checkbox" name="collapse" id="global-checkboxcollapse-{ID}"><label for="global-checkboxcollapse-{ID}">'+_('Collapse code blocks on startup.')+'</label></div> \
+							</div> \
+						</div> \
+						<div class="card-action"> \
+							<a onclick="toggleExpandable(\'global-advanced-{ID}\',\'shareurl-advanced\');">'+_('Advanced Options')+'</a> \
+						</div> \
+					</div> \
+				</div> \
+            </div> \
+        </div> \
+    </div> \
+</div>';
+	this.render = function(){
+		//TODO: Have a TB placeholder image specific to TB projects
+		var html = this.renderData.replace(new RegExp('\{ID\}', 'g'), this.id);
+		var frag = document.createRange().createContextualFragment(html);
+		
+		//set image
+		if (this.ProjectData.ProjectImage!=null&&this.ProjectData.ProjectImage!=""){
+			frag.getElementById("global-project-image-"+this.id).src = this.ProjectData.ProjectImage;
+		} else {
+			frag.getElementById("global-project-image-"+this.id).src = this.PlaceholderImage;
+		}
+
+		//set tags
+		var tagcontainer = frag.getElementById("global-project-tags-"+this.id);
+		for (var i = 0; i<this.ProjectData.ProjectTags.length; i++){
+			var chip = document.createElement("div");
+			chip.classList.add("chipselect");
+			chip.textContent = Planet.TagsManifest[this.ProjectData.ProjectTags[i]].TagName;
+			tagcontainer.appendChild(chip);
+		}
+		
+		//set title text
+		frag.getElementById("global-project-title-"+this.id).textContent = this.ProjectData.ProjectName;
+
+		//set number of likes
+		frag.getElementById("global-project-likes-"+this.id).textContent = this.ProjectData.ProjectLikes.toString();
+
+		//set view button listener
+		var t = this;
+		frag.getElementById("global-project-more-details-"+this.id).addEventListener('click', function (evt) {
+			Planet.GlobalPlanet.ProjectViewer.open(t.id);
+		});
+
+		//set image listener
+		frag.getElementById("global-project-image-"+this.id).addEventListener('click', function (evt) {
+			Planet.GlobalPlanet.ProjectViewer.open(t.id);
+		});
+
+		//set share button listener
+		frag.getElementById("global-project-share-"+this.id).addEventListener('click', function (evt) {
+			var s = document.getElementById("global-sharebox-"+t.id);
+			if (s.style.display=="none"){
+				s.style.display = "initial";
+				hideOnClickOutside(document.getElementById("global-share-"+t.id), "global-sharebox-"+t.id);
+			} else {
+				s.style.display = "none";
+			}
+		});
+
+		//set share checkbox listener
+		frag.getElementById("global-checkboxrun-"+this.id).addEventListener('click', function (evt) {
+			updateCheckboxes("global-sharebox-"+t.id);
+		});
+		frag.getElementById("global-checkboxshow-"+this.id).addEventListener('click', function (evt) {
+			updateCheckboxes("global-sharebox-"+t.id);
+		});
+		frag.getElementById("global-checkboxcollapse-"+this.id).addEventListener('click', function (evt) {
+			updateCheckboxes("global-sharebox-"+t.id);
+		});
+
+		//set like icon
+		if (Planet.ProjectStorage.isLiked(this.id)){
+			frag.getElementById("global-like-icon-"+this.id).textContent = "favorite";
+		} else {
+			frag.getElementById("global-like-icon-"+this.id).textContent = "favorite_border";
+		}
+
+		frag.getElementById("global-like-icon-"+this.id).addEventListener('click', function (evt) {
+			t.like();
+		});
+
+		document.getElementById("global-projects").appendChild(frag);
+		updateCheckboxes("global-sharebox-"+t.id);
+	};
+
+	this.like = function(){
+		var like = true;
+		if (Planet.ProjectStorage.isLiked(this.id)){
+			like = false;
+		}
+		Planet.ServerInterface.likeProject(this.id,like,function(data){this.afterLike(data,like)}.bind(this));
+	};
+
+	this.afterLike = function(data,like){
+		if (!data.success){
+			if (data.error=="ERROR_ACTION_NOT_PERMITTED"){
+				this.setLike(like);
+			}
+		} else {
+			this.setLike(like);
+		}
+	};
+
+	this.setLike = function(like){
+		Planet.ProjectStorage.like(this.id,like);
+		var incr = 1;
+		var text = "favorite";
+		if (!like){
+			incr = -1;
+			text = "favorite_border";
+		}
+		var l = document.getElementById("global-project-likes-"+this.id);
+		l.textContent = (parseInt(l.textContent)+incr).toString();
+		document.getElementById("global-like-icon-"+this.id).textContent = text;
+	};
+
+	this.init = function(id){
+		this.id = id;
+		this.ProjectData = Planet.GlobalPlanet.cache[id];
+	};
+};
---
+++ planet/js/ProjectStorage.js
@@ -0,0 +1,168 @@
+// Copyright (c) 2017 Euan Ong
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the The GNU Affero General Public
+// License as published by the Free Software Foundation; either
+// version 3 of the License, or (at your option) any later version.
+//
+// You should have received a copy of the GNU Affero General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, 51 Franklin Street, Suite 500 Boston, MA 02110-1335 USA
+
+function ProjectStorage(Planet){
+	this.defaultProjectName = _("My Project");
+	this.LocalStorage = null;
+	this.data = null;
+	this.LocalStorageKey = "ProjectData";
+
+	this.generateID = function(){
+		var n = Date.now();
+		var prefix = n.toString();
+		var suffix = ""
+		for (var i = 0; i<3; i++){
+			suffix+=Math.floor(Math.random()*10).toString();
+		}
+		return prefix+suffix;
+	};
+
+	this.saveLocally = function(data, image){
+		if (this.data.CurrentProject===undefined){
+			this.initialiseNewProject();
+		}
+		var c = this.data.CurrentProject;
+		if (this.data.Projects[c]===undefined){
+			this.data.Projects[c]={};
+			this.data.Projects[c].ProjectName = this.defaultProjectName;
+			this.data.Projects[c].ProjectData = null;
+			this.data.Projects[c].ProjectImage = null;
+			this.data.Projects[c].PublishedData = null;
+			this.data.Projects[c].DateLastModified = Date.now();
+		}
+		this.data.Projects[c].ProjectData = data;
+		this.data.Projects[c].ProjectImage = image;
+		this.data.Projects[c].DateLastModified = Date.now();
+		this.save();
+	};
+
+	this.getCurrentProjectData = function(){
+		var c = this.data.CurrentProject;
+		if (this.data.Projects[c]===undefined){
+			return null;
+		} else {
+			return this.data.Projects[c].ProjectData;
+		}
+	}
+
+	this.initialiseNewProject = function(name,data,image){
+		if (name===undefined){
+			name=this.defaultProjectName;
+		}
+		if (data===undefined){
+			data=null;
+		}
+		if (image===undefined){
+			image=null;
+		}
+		var c = this.generateID();
+		this.data.CurrentProject = c;
+		this.data.Projects[c]={};
+		this.data.Projects[c].ProjectName = name;
+		this.data.Projects[c].ProjectData = data;
+		this.data.Projects[c].ProjectImage = image;
+		this.data.Projects[c].PublishedData = null;
+		this.data.Projects[c].DateLastModified = Date.now();
+		this.save();
+	};
+
+	this.renameProject = function(id, name){
+		this.data.Projects[id].ProjectName = name;
+		this.save();
+	};
+
+	this.addPublishedData = function(id, data){
+		this.data.Projects[id].PublishedData = data;
+		this.save();
+	};
+
+	this.deleteProject = function(id){
+		delete this.data.Projects[id];
+		this.save();
+		Planet.LocalPlanet.updateProjects();
+	};
+
+	this.getCurrentProjectID = function(){
+		return this.data.CurrentProject;
+	};
+
+	this.setCurrentProjectID = function(id){
+		this.data.CurrentProject = id;
+	};
+
+	this.encodeTB = function(tb){
+		return window.btoa(encodeURIComponent(tb));
+	};
+
+	this.decodeTB = function(tb){
+		return decodeURIComponent(window.atob(tb));
+	};
+
+	this.isLiked = function(id){
+		if (this.data.LikedProjects[id]==true){
+			return true;
+		}
+		return false;
+	};
+
+	this.like = function(id,like){
+		this.data.LikedProjects[id]=like;
+		this.save();
+	};
+
+	//Ancillary Functions
+
+	this.set = function(key, obj){
+		var jsonobj = JSON.stringify(obj);
+		this.LocalStorage.setItem(key,jsonobj);
+	};
+
+	this.get = function(key){
+		var jsonobj = this.LocalStorage.getItem(key);
+		if (jsonobj===null||jsonobj===""){
+			return null;
+		}
+		try {
+			return JSON.parse(jsonobj);
+		} catch (e){
+			console.log(e);
+			return null;
+		}
+	};
+
+	this.save = function(){
+		this.set(this.LocalStorageKey,this.data);
+	};
+
+	this.restore = function(){
+		this.data = this.get(this.LocalStorageKey);
+	};
+
+	this.initialiseStorage = function(){
+		this.data = {};
+		this.data.Projects = {};
+		this.data.LikedProjects = {};
+		this.data.DefaultCreatorName = _("anonymous");
+		this.save();
+	};
+
+	this.getDefaultCreatorName = function(){
+		return this.data.DefaultCreatorName;
+	};
+
+	this.init = function(){
+		this.LocalStorage = Planet.LocalStorage;
+		this.restore();
+		if (this.data===null){
+			this.initialiseStorage();
+		}
+	};
+};
