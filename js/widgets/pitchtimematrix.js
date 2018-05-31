627e5b079006eb4a89a43e894b461c624aeb4251
Walter Bender <walter.bender@gmail.com>
2018-05-31 08:36:36 -0400

    use instance variable instead of global; use boolean value

--- js/widgets/pitchtimematrix.js
+++ js/widgets/pitchtimematrix.js
@@ -14,7 +14,6 @@ const MATRIXGRAPHICS = ['forward', 'back', 'right', 'left', 'setcolor', 'setshad
 const MATRIXGRAPHICS2 = ['arc', 'setxy'];
 // Deprecated
 const MATRIXSYNTHS = ['sine', 'triangle', 'sawtooth', 'square', 'hertz'];
-var stopOrCloseClicked = 0;
 
 function PitchTimeMatrix () {
     const BUTTONDIVWIDTH = 476;  // 8 buttons 476 = (55 + 4) * 8
@@ -22,6 +21,8 @@ function PitchTimeMatrix () {
     const INNERWINDOWWIDTH = 600;
     const BUTTONSIZE = 53;
     const ICONSIZE = 32;
+
+    this._stopOrCloseClicked = false;
 
     this.paramsEffects = {
         "doVibrato": false,
@@ -211,7 +212,7 @@ function PitchTimeMatrix () {
 
             that._logo.synth.stopSound(0, 'default');
             that._logo.synth.stop();
-            stopOrCloseClicked = 1;
+            that._stopOrCloseClicked = true;
             ptmTableDiv.style.visibility = 'hidden';
             ptmButtonsDiv.style.visibility = 'hidden';
             ptmDiv.style.visibility = 'hidden';
@@ -1245,7 +1246,8 @@ function PitchTimeMatrix () {
                         pitchNotes.push(note[i].replace(/♭/g, 'b').replace(/♯/g, '#'));
                     }
                 }
-                stopOrCloseClicked = 0;
+
+                this._stopOrCloseClicked = false;
             }
 
             var noteValue = this._notesToPlay[this._notesCounter][1];
@@ -1289,7 +1291,7 @@ function PitchTimeMatrix () {
 
             this.__playNote(0, 0, playButtonCell);
         } else {
-        	  stopOrCloseClicked = 1;
+            this._stopOrCloseClicked = true;
             playButtonCell.innerHTML = '&nbsp;&nbsp;<img src="header-icons/' + 'play-button.svg' + '" title="' + _('play') + '" alt="' + _('play') + '" height="' + ICONSIZE + '" width="' + ICONSIZE + '" vertical-align="middle" align-content="center">&nbsp;&nbsp;';
         }
     };
@@ -1354,39 +1356,38 @@ function PitchTimeMatrix () {
                 var drumNotes = [];
 
                 // Note can be a chord, hence it is an array.
-                if (stopOrCloseClicked == 0) {
-	                for (var i = 0; i < note.length; i++) {
-	                    if (typeof(note[i]) === 'number') {
-	                        var drumName = null;
-	                    } else {
-	                        var drumName = getDrumName(note[i]);
-	                    }
+                if (!that._stopOrCloseClicked) {
+                    for (var i = 0; i < note.length; i++) {
+                        if (typeof(note[i]) === 'number') {
+                            var drumName = null;
+                        } else {
+                            var drumName = getDrumName(note[i]);
+                        }
 
-	                    if (typeof(note[i]) === 'number') {
-	                        synthNotes.push(note[i]);
-	                    } else if (drumName != null) {
-	                        drumNotes.push(drumName);
-	                    } else if (note[i].slice(0, 4) === 'http') {
-	                        drumNotes.push(note[i]);
-	                    } else {
-	                        var obj = note[i].split(':');
-	                        // Deprecated
-	                        if (MATRIXSYNTHS.indexOf(obj[0]) !== -1) {
-	                            synthNotes.push(note[i]);
-	                            continue;
-	                        } else if (MATRIXGRAPHICS.indexOf(obj[0]) !== -1) {
-	                            that._processGraphics(obj);
-	                        } else if (MATRIXGRAPHICS2.indexOf(obj[0]) !== -1) {
-	                            that._processGraphics(obj);
-	                        } else {
-	                            pitchNotes.push(note[i].replace(/♭/g, 'b').replace(/♯/g, '#'));
-	                        }
-	                    }
-	                }
-               	}
+                        if (typeof(note[i]) === 'number') {
+                            synthNotes.push(note[i]);
+                        } else if (drumName != null) {
+                            drumNotes.push(drumName);
+                        } else if (note[i].slice(0, 4) === 'http') {
+                            drumNotes.push(note[i]);
+                        } else {
+                            var obj = note[i].split(':');
+                            // Deprecated
+                            if (MATRIXSYNTHS.indexOf(obj[0]) !== -1) {
+                                synthNotes.push(note[i]);
+                                continue;
+                            } else if (MATRIXGRAPHICS.indexOf(obj[0]) !== -1) {
+                                that._processGraphics(obj);
+                            } else if (MATRIXGRAPHICS2.indexOf(obj[0]) !== -1) {
+                                that._processGraphics(obj);
+                            } else {
+                                pitchNotes.push(note[i].replace(/♭/g, 'b').replace(/♯/g, '#'));
+                            }
+                        }
+                    }
+                       }
 
                 if (note[0] !== 'R' && pitchNotes.length > 0) {
-
                     that._logo.synth.trigger(0, pitchNotes, that._logo.defaultBPMFactor / noteValue, 'default', null, null);
                 }
 
@@ -1396,7 +1397,6 @@ function PitchTimeMatrix () {
 
                 for (var i = 0; i < drumNotes.length; i++) {
                     that._logo.synth.trigger(0, ['C2'], that._logo.defaultBPMFactor / noteValue, drumNotes[i], null, null);
-
                 }
             }
 
@@ -1424,7 +1424,7 @@ function PitchTimeMatrix () {
                 }
             }
         }, that._logo.defaultBPMFactor * 1000 * time + that._logo.turtleDelay);
-	  };
+    };
 
     this._processGraphics = function (obj) {
         switch(obj[0]) {
