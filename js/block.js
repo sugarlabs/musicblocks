45a0d8fe7ce438c4ae440adf2b67bc7cbe0e25b3
Walter Bender <walter.bender@gmail.com>
2018-06-05 23:06:58 -0400

    no 0 in scale degree

--- js/block.js
+++ js/block.js
@@ -1865,7 +1865,7 @@ function Block(protoblock, blocks, overrideName) {
                     this._piemenuNumber([-3, -2, -1, 0, 1, 2, 3], this.value);
                     break;
                 case 'scaledegree':
-                    this._piemenuNumber([-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7], this.value);
+                    this._piemenuNumber([-7, -6, -5, -4, -3, -2, -1, 1, 2, 3, 4, 5, 6, 7], this.value);
                     break;
                 case 'meter':
                     this._piemenuNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16], this.value);
