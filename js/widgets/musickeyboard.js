8ddc5f3114c13bf38fd70bf7fd1a7d4b5d1e63bc
a-ritwik <a.ritwik@iitg.ac.in>
2018-06-21 00:05:35 +0530

    removed toggle button

--- js/widgets/musickeyboard.js
+++ js/widgets/musickeyboard.js
@@ -9,16 +9,6 @@
 // License along with this library; if not, write to the Free Software
 // Foundation, 51 Franklin Street, Suite 500 Boston, MA 02110-1335 USA
 
-//elements
-
-// function myFunction() {
-//     var x = document.getElementById("keyboardHolder");
-//     if (x.style.display === "none") {
-//         x.style.display = "block";
-//     } else {
-//         x.style.display = "none";
-//     }
-// }
 
 function MusicKeyboard() {
 
