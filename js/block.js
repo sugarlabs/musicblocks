c410e37ef8cbb1008e066c228436909e1254989c
Riya Lohia <riyalh1997@gmail.com>
2018-08-03 21:11:03 +0530

    Added show/hide handle for saved stack

--- js/block.js
+++ js/block.js
@@ -14,7 +14,7 @@
 const TEXTWIDTH = 240; // 90
 const STRINGLEN = 9;
 const LONGPRESSTIME = 1500;
-const COLLAPSABLES = ['drum', 'start', 'action', 'matrix', 'pitchdrummatrix', 'rhythmruler', 'timbre', 'status', 'pitchstaircase', 'tempo', 'pitchslider', 'modewidget'];
+const COLLAPSABLES = ['drum', 'start', 'action', 'matrix', 'temperament1', 'pitchdrummatrix', 'rhythmruler', 'timbre', 'status', 'pitchstaircase', 'tempo', 'pitchslider', 'modewidget'];
 const NOHIT = ['hidden', 'hiddennoflow'];
 const SPECIALINPUTS = ['text', 'number', 'solfege', 'customNote', 'eastindiansolfege', 'notename', 'voicename', 'modename', 'drumname', 'filtertype', 'oscillatortype', 'boolean', 'intervalname', 'invertmode', 'accidentalname', 'temperamentname'];
 const WIDENAMES = ['intervalname', 'accidentalname', 'drumname', 'voicename', 'modename', 'temperamentname', 'modename'];
@@ -756,6 +756,9 @@ function Block(protoblock, blocks, overrideName) {
                     break;
                 case 'drum':
                     that.collapseText = new createjs.Text(_('drum'), fontSize + 'px Sans', '#000000');
+                    break;
+                case 'temperament1':
+                    that.collapseText = new createjs.Text(_('temperament'), fontSize + 'px Sans', '#000000');
                     break;
                 }
 
