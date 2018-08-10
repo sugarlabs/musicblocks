21c74d7e17bca1f4d24427af7e7de21b83a325a6
eohomegrownapps <euan.l.y.ong@gmail.com>
2018-08-10 21:51:28 +0100

    Indentation update
    As in title

--- planet/js/StringHelper.js
+++ planet/js/StringHelper.js
@@ -10,61 +10,61 @@
 // Foundation, 51 Franklin Street, Suite 500 Boston, MA 02110-1335 USA
 
 function StringHelper(Planet) {
-	//[id, string, property (if present)]
-	// append to innerhtml
+    //[id, string, property (if present)]
+    // append to innerhtml
     this.strings = [
-    	["logo-container","Planet"],
-    	["close-planet","Close Planet","data-tooltip"],
-    	["planet-open-file","Open project from file","data-tooltip"],
-    	["planet-new-project","New project","data-tooltip"],
-    	["local-tab","Local"],
-    	["global-tab","Global"],
-    	["global-search","Search for a project","placeholder"],
-    	["localtitle","My Projects"],
-    	["publisher-ptitle","Publish Project"],
-    	["publish-title-label","Project title"],
-    	["publish-tags-label","Tags (max 5)"],
-    	["publish-description-label","Description"],
-    	["publisher-submit","Submit"],
-    	["publisher-cancel","Cancel"],
-    	["deleter-confirm","Delete \"<span id=\"deleter-title\"></span>\"?"],
-    	["deleter-paragraph","Permanently delete project \"<span id=\"deleter-name\"></span>\"?"],
-    	["deleter-button","Delete"],
-    	["deleter-cancel","Cancel"],
-    	["globaltitle","Explore Projects"],
-    	["view-more-chips","View More"],
-    	["option-recent","Most recent"],
-    	["option-liked","Most liked"],
-    	["option-downloaded","Most downloaded"],
-    	["option-alphabetical","A-Z"],
-    	["option-sort-by","Sort by"],
-    	["load-more-projects","Load More Projects"],
-    	["projectviewer-last-updated-heading","Last Updated"],
-    	["projectviewer-date-heading","Creation Date"],
-    	["projectviewer-downloads-heading","Number of Downloads:"],
-    	["projectviewer-likes-heading","Number of Likes:"],
-    	["projectviewer-tags-heading","Tags:"],
-    	["projectviewer-description-heading","Description"],
-    	["projectviewer-report-project","Report Project"],
-    	["projectviewer-report-title","Report Project"],
-    	["projectviewer-report-conduct","Report projects which violate the <a href=\"https://github.com/sugarlabs/sugar-docs/blob/master/CODE_OF_CONDUCT.md\" target=\"_blank\">Sugar Labs Code of Conduct</a>."],
-    	["projectviewer-report-reason","Reason for reporting project"],
-    	["projectviewer-report-submit","Submit"],
-		["projectviewer-reportsubmit-title","Report Project"],
-		["projectviewer-report-close","Close"],
-		["projectviewer-download-file","Download as File"],
-		["projectviewer-open-mb","Open in Music Blocks"]   	
+        ["logo-container","Planet"],
+        ["close-planet","Close Planet","data-tooltip"],
+        ["planet-open-file","Open project from file","data-tooltip"],
+        ["planet-new-project","New project","data-tooltip"],
+        ["local-tab","Local"],
+        ["global-tab","Global"],
+        ["global-search","Search for a project","placeholder"],
+        ["localtitle","My Projects"],
+        ["publisher-ptitle","Publish Project"],
+        ["publish-title-label","Project title"],
+        ["publish-tags-label","Tags (max 5)"],
+        ["publish-description-label","Description"],
+        ["publisher-submit","Submit"],
+        ["publisher-cancel","Cancel"],
+        ["deleter-confirm","Delete \"<span id=\"deleter-title\"></span>\"?"],
+        ["deleter-paragraph","Permanently delete project \"<span id=\"deleter-name\"></span>\"?"],
+        ["deleter-button","Delete"],
+        ["deleter-cancel","Cancel"],
+        ["globaltitle","Explore Projects"],
+        ["view-more-chips","View More"],
+        ["option-recent","Most recent"],
+        ["option-liked","Most liked"],
+        ["option-downloaded","Most downloaded"],
+        ["option-alphabetical","A-Z"],
+        ["option-sort-by","Sort by"],
+        ["load-more-projects","Load More Projects"],
+        ["projectviewer-last-updated-heading","Last Updated"],
+        ["projectviewer-date-heading","Creation Date"],
+        ["projectviewer-downloads-heading","Number of Downloads:"],
+        ["projectviewer-likes-heading","Number of Likes:"],
+        ["projectviewer-tags-heading","Tags:"],
+        ["projectviewer-description-heading","Description"],
+        ["projectviewer-report-project","Report Project"],
+        ["projectviewer-report-title","Report Project"],
+        ["projectviewer-report-conduct","Report projects which violate the <a href=\"https://github.com/sugarlabs/sugar-docs/blob/master/CODE_OF_CONDUCT.md\" target=\"_blank\">Sugar Labs Code of Conduct</a>."],
+        ["projectviewer-report-reason","Reason for reporting project"],
+        ["projectviewer-report-submit","Submit"],
+        ["projectviewer-reportsubmit-title","Report Project"],
+        ["projectviewer-report-close","Close"],
+        ["projectviewer-download-file","Download as File"],
+        ["projectviewer-open-mb","Open in Music Blocks"]    
     ]
 
     this.init = function(){
         for (var i = 0; i<this.strings.length; i++){
-        	var obj = this.strings[i];
-        	var elem = document.getElementById(obj[0]);
-        	if (this.strings[i].length==3){
-        		elem.setAttribute(obj[2],obj[1]);
-        	} else {
-        		elem.innerHTML+=obj[1];
-        	}
+            var obj = this.strings[i];
+            var elem = document.getElementById(obj[0]);
+            if (this.strings[i].length==3){
+                elem.setAttribute(obj[2],obj[1]);
+            } else {
+                elem.innerHTML+=obj[1];
+            }
         }
     };
 };
