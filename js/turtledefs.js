2c15ea857bde22fe23e8d2fc6e8f2132762b4fe0
Walter Bender <walter.bender@gmail.com>
2018-09-26 12:59:37 -0400

    add step pitch block

--- js/turtledefs.js
+++ js/turtledefs.js
@@ -43,7 +43,7 @@ function beginnerBlock(name) {
     // Only these blocks appear on the palette in beginner mode.
     return ['newnote', 'note4', 'rest2', 'mynotevalue',  // notes palette
             'meter', 'setmasterbpm2', 'everybeatdo', 'beatvalue',  // meter palette
-            'pitch', 'pitch2', 'pitchnumber', 'hertz', 'fourth', 'fifth', 'mypitch', 'pitchinhertz', // pitch palette
+            'pitch', 'pitch2', 'pitchnumber', 'hertz', 'steppitch', 'fourth', 'fifth', 'mypitch', 'pitchinhertz', // pitch palette
             'setkey2', 'modelength', 'thirdinterval', 'sixthinterval', 'chordI', 'chordIV', 'chordV', // interval palette
             'settimbre', 'newstaccato', 'newslur', 'vibrato', 'neighbor2', 'chorus', 'dis', 'phaser', 'tremelo', // tone palette
             'crescendo', 'decrescendo', 'setsynthvolume',  // volume palette
