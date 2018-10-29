7989f17f15c0935414ee984550b30625237873b2
Nathan Jacob <37455968+nathan-jacob@users.noreply.github.com>
2018-10-28 23:01:32 -0500

    Fixes Pallette Bar hiding on search
    Fixes Pallette Bar hiding on search, satisfies gci task https://codein.withgoogle.com/tasks/4750769602428928/?sp-organization=5730203977383936&sp-categories=1&sp-is_beginner=False

--- js/activity.js
+++ js/activity.js
@@ -2464,10 +2464,7 @@ define(MYDEFINES, function (compatibility) {
                 if (searchResult) {
                     palettes.dict[paletteName].makeBlockFromSearch(protoblk, protoName, function (newBlock) {
                         blocks.moveBlock(newBlock, 100 + searchBlockPosition[0] - blocksContainer.x, searchBlockPosition[1] - blocksContainer.y);
-                        // Race condition with palette hide.
-                        setTimeout(function() {
-                            palettes.show();
-                        }, 200);
+                        
                     });
 
                     // Move the position of the next newly created block.
@@ -2595,7 +2592,7 @@ define(MYDEFINES, function (compatibility) {
             // Check for RETURN in search widget ahead of other events.
             if (event.keyCode === RETURN && docById('search').value.length > 0) {
                 doSearch();
-                palettes.hide();
+                //palettes.hide();
                 // palettes.show();
             }
 
