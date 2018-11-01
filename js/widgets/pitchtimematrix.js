ce5fb71f7c772eb6fb50f923b7de8f05e5ee4cef
Walter Bender <walter.bender@gmail.com>
2018-11-01 08:20:19 -0400

    deprecate accidental block

--- js/widgets/pitchtimematrix.js
+++ js/widgets/pitchtimematrix.js
@@ -1789,6 +1789,8 @@ function PitchTimeMatrix () {
                                 previousBlock = thisBlock;
                                 thisBlock += 3;
                             } else {
+                                // Accidental block is deprecated
+                                /*
                                 newStack.push([thisBlock, 'accidental', 0, 0, [previousBlock, thisBlock + 1, thisBlock + 2, thisBlock + 5]]);
                                 newStack.push([thisBlock + 1, ['accidentalname', {value: _('sharp') + ' ♯'}], 0, 0, [thisBlock]]);
                                 newStack.push([thisBlock + 2, 'pitch', 0, 0, [thisBlock, thisBlock + 3, thisBlock + 4, null]]);
@@ -1800,6 +1802,12 @@ function PitchTimeMatrix () {
                                 newStack.push([thisBlock + 5, 'hidden', 0, 0, [thisBlock, lastConnection]]);
                                 previousBlock = thisBlock + 5;
                                 thisBlock += 6;
+                                */
+                                newStack.push([thisBlock, 'pitch', 0, 0, [previousBlock, thisBlock + 1, thisBlock + 2, lastConnection]]);
+                                newStack.push([thisBlock + 1, ['solfege', {'value': SOLFEGECONVERSIONTABLE[note[0][j][0] + '♯']}], 0, 0, [thisBlock]]);
+                                newStack.push([thisBlock + 2, ['number', {'value': note[0][j][2]}], 0, 0, [thisBlock]]);
+                                previousBlock = thisBlock;
+                                thisBlock += 3;
                             }
                         } else if (note[0][j][1] === '♭') {
                             if (this._logo.synth.inTemperament == 'custom') {
@@ -1809,6 +1817,8 @@ function PitchTimeMatrix () {
                                 previousBlock = thisBlock;
                                 thisBlock += 3;
                             } else {
+                                // Accidental block is deprecated
+                                /*
                                 newStack.push([thisBlock, 'accidental', 0, 0, [previousBlock, thisBlock + 1, thisBlock + 2, thisBlock + 5]]);
                                 newStack.push([thisBlock + 1, ['accidentalname', {value: _('flat') + ' ♭'}], 0, 0, [thisBlock]]);
                                 newStack.push([thisBlock + 2, 'pitch', 0, 0, [thisBlock, thisBlock + 3, thisBlock + 4, null]]);    
@@ -1820,6 +1830,12 @@ function PitchTimeMatrix () {
                                 newStack.push([thisBlock + 5, 'hidden', 0, 0, [thisBlock, lastConnection]]);
                                 previousBlock = thisBlock + 5;
                                 thisBlock += 6;
+                                */
+                                newStack.push([thisBlock, 'pitch', 0, 0, [previousBlock, thisBlock + 1, thisBlock + 2, lastConnection]]);
+                                newStack.push([thisBlock + 1, ['solfege', {'value': SOLFEGECONVERSIONTABLE[note[0][j][0] + '♭']}], 0, 0, [thisBlock]]);
+                                newStack.push([thisBlock + 2, ['number', {'value': note[0][j][2]}], 0, 0, [thisBlock]]);
+                                previousBlock = thisBlock;
+                                thisBlock += 3;
                             }    
                         } else {
                             newStack.push([thisBlock, 'pitch', 0, 0, [previousBlock, thisBlock + 1, thisBlock + 2, lastConnection]]);
