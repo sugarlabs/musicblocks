4f6fc0b610b922f09d2ebde399ba2799eff525ab
sarthakagr104 <44672053+sarthakagr104@users.noreply.github.com>
2018-11-28 16:30:53 +0530

    Docstring with {}

--- js/block.js
+++ js/block.js
@@ -345,8 +345,8 @@ function Block(protoblock, blocks, overrideName) {
         // artwork and recalculate the hitarea.
         var that = this;
 
-        //if bitmap is not equal to null then Position media & Sets child
-        //@param-that = this = resize function
+        /*if bitmap is not equal to null then Position media & Sets child
+        @param-that = this = resize function*/
         this.postProcess = function (that) {
             if (that.imageBitmap !== null) {
                 that._positionMedia(that.imageBitmap, that.imageBitmap.image.width, that.imageBitmap.image.height, scale);
@@ -388,8 +388,8 @@ function Block(protoblock, blocks, overrideName) {
 
         if (this.container !== null) {
             var that = this;
-            // function to collapse Button Bitmap and expand button bitmap to scaleX nad scaleY update cache and calculate block hit area
-            //@param-that = this = container
+            /* function to collapse Button Bitmap and expand button bitmap to scaleX nad scaleY update cache and calculate block hit area
+            @param-that = this = container*/
             var _postProcess = function (that) {
                 that.collapseButtonBitmap.scaleX = that.collapseButtonBitmap.scaleY = that.collapseButtonBitmap.scale = scale / 3;
                 that.expandButtonBitmap.scaleX = that.expandButtonBitmap.scaleY = that.expandButtonBitmap.scale = scale / 3;
@@ -406,7 +406,8 @@ function Block(protoblock, blocks, overrideName) {
             }
         }
     };
-//check if collapsible and if name = interval
+/*check if collapsible and if name = interval
+@param-plusMinus-new variable*/
     this._newArtwork = function (plusMinus) {
         if (this.isCollapsible()) {
             var proto = new ProtoBlock('collapse');
@@ -854,8 +855,8 @@ function Block(protoblock, blocks, overrideName) {
             }
         }
     };
-//generateCollapseArtwork
-    //param postProcess = null
+/*generateCollapseArtwork
+    param postProcess = null*/
     this._generateCollatpseArt work = function (postProcess) {
         var that = this;
         var thisBlock = this.blocks.blockList.indexOf(this);
@@ -896,8 +897,8 @@ function Block(protoblock, blocks, overrideName) {
 
             image.src = 'data:image/svg+xml;base64,' + window.btoa(unescape(encodeURIComponent(COLLAPSEBUTTON)));
         };
-//if line is collapsible then expand button bitmap to 4 protoblock or else expand button bitmap to 10 protoblock
-         //@param - that = generateCollapseArt
+/*if line is collapsible then expand button bitmap to 4 protoblock or else expand button bitmap to 10 protoblock
+         @param - that = generateCollapseArt*/
         var __processExpandButton = function (that) {
             var image = new Image();
             image.onload = function () {
@@ -919,8 +920,9 @@ function Block(protoblock, blocks, overrideName) {
 
             image.src = 'data:image/svg+xml;base64,' + window.btoa(unescape(encodeURIComponent(EXPANDBUTTON)));
         };
-//check if collapsed is null and switch the name
-        
+/*check if collapsed is null and switch the name
+  @param-bitmap-null
+  @param-that-generateCollapseArt*/
         var __processHighlightCollapseBitmap = function (bitmap, that) {
             that.highlightCollapseBlockBitmap = bitmap;
             that.highlightCollapseBlockBitmap.name = 'highlight_collapse_' + thisBlock;
@@ -1000,8 +1002,8 @@ function Block(protoblock, blocks, overrideName) {
 
             __processExpandButton(that);
         };
-//collapse block bitmap 
-        //@param - bitmap - null
+/*collapse block bitmap 
+        @param - bitmap - null*/
         var __processCollapseBitmap = function (bitmap, that) {
             that.collapseBlockBitmap = bitmap;
             that.collapseBlockBitmap.name = 'collapse_' + thisBlock;
@@ -1589,8 +1591,8 @@ function Block(protoblock, blocks, overrideName) {
 
         this.blocks.refreshCanvas();
     };
-//customize text
-    
+/*customize text
+  @param-blockscale-scale*/  
     this._positionText = function (blockScale) {
         this.text.textBaseline = 'alphabetic';
         this.text.textAlign = 'right';
@@ -1630,11 +1632,11 @@ function Block(protoblock, blocks, overrideName) {
         this.container.setChildIndex(this.text, z);
         this.updateCache();
     };
-    //@param-bitmap - image
-    //@param-width-width of canvas
-    //@param-height-height of canvas
-    //@param-blockscale-scale
-//if width is grater than height, then width * blockscale else height * blockscale
+    /*@param-bitmap - image
+    @param-width-width of canvas
+    @param-height-height of canvas
+    @param-blockscale-scale
+if width is grater than height, then width * blockscale else height * blockscale*/
     this._positionMedia = function (bitmap, width, height, blockScale) {
         if (width > height) {
             bitmap.scaleX = bitmap.scaleY = bitmap.scale = MEDIASAFEAREA[2] / width * blockScale / 2;
@@ -1915,11 +1917,11 @@ function Block(protoblock, blocks, overrideName) {
             moved = false;
         });
     };
-    //@param-event- mouse
-    //@param-moved-cursor moved
-    //@param-haveClick-when clickd
-    //@param-hideDOM-hide mouse
-//set cursor style to default
+    /*@param-event- mouse
+    @param-moved-cursor moved
+    @param-haveClick-when clickd
+    @param-hideDOM-hide mouse
+set cursor style to default*/
     this._mouseoutCallback = function (event, moved, haveClick, hideDOM) {
         var thisBlock = this.blocks.blockList.indexOf(this);
         if (!this.blocks.logo.runningLilypond) {
