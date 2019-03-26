fd22bfb1a4bc2062985669e7cd6b4654213cf70f
Walter Bender <walter.bender@gmail.com>
2019-03-26 16:55:19 -0400

    update pitch slider instructions

--- js/widgets/pitchslider.js
+++ js/widgets/pitchslider.js
@@ -399,6 +399,6 @@ function PitchSlider() {
             };
         }
 
-        this._logo.textMsg(_('Use the slider to change the pitch.'));
+        this._logo.textMsg(_('Click on the slider to create a note block.'));
     };
 };
