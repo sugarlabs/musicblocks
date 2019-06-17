5176eb08e21e89db42e68af13eacd6e0cb48b53a
Favour Kelvin <favourkelvin17@gmail.com>
2019-06-17 11:26:11 +0100

    added phantom.js prebuilt as dev dependecies and amend node.js version of travis.yml

--- karma.conf.js
+++ karma.conf.js
@@ -47,7 +47,7 @@ module.exports = function(config) {
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: [
-    //   'Chrome',
+      // 'Chrome',
       'PhantomJS'
     ],
     
