93bd5031064f65c9c5472ffff587be7e2b8314da
Favour Kelvin <favourkelvin17@gmail.com>
2019-06-21 21:54:35 +0100

    made changes

--- tests/promiseexecution.js
+++ tests/promiseexecution.js
@@ -2,11 +2,47 @@
  * Test the function
  */
 
-describe("Drag Drop widget", function() {
-    it("Draggable has fout items", function() {
-      expect(dragDrop.draggable.length).toBe(4);
-    });
-    it("This widget has two properties", function() {
-      expect(Object.keys(dragDrop).length).toBe(2);
-    });
+// describe("Testing async functions", function() {
+  
+//   it("Should delay for awhile", async function(){
+//     let flag = false;
+//     flag = await delayExecution();
+//     expect(flag).toBeTruthy();
+//   }); 
+  
+// });
+
+
+
+
+// describe("Testing async functions", function() {
+  
+//   beforeEach(function() {
+//     return new Promise(function(resolve, reject) {
+//       // do something asynchronous
+//       resolve();
+//     });
+//   });
+  
+//   it('does a thing', function() {
+//     return someAsyncFunction().then(function (result) {
+//       expect(result).toEqual(someExpectedValue);
+//     });
+//   });
+
+// });
+
+
+describe("Testing async functions", function() {
+  
+  beforeEach(async function() {
+    await delayExecution();
   });
+  
+  it('it should delay for few minutes', async function() {
+    let completed = false;
+    completed  = await delayExecution();
+    expect(completed).toEqual(true);
+  });
+  
+});
