c5cce3d8c33cf91dbf7a126b9ff9e7a2adbed8ef
Favour Kelvin <favourkelvin17@gmail.com>
2019-06-21 22:10:33 +0100

    removed unused code

--- tests/promiseexecution.js
+++ tests/promiseexecution.js
@@ -2,44 +2,13 @@
  * Test the function
  */
 
-// describe("Testing async functions", function() {
-  
-//   it("Should delay for awhile", async function(){
-//     let flag = false;
-//     flag = await delayExecution();
-//     expect(flag).toBeTruthy();
-//   }); 
-  
-// });
-
-
-
-
-// describe("Testing async functions", function() {
-  
-//   beforeEach(function() {
-//     return new Promise(function(resolve, reject) {
-//       // do something asynchronous
-//       resolve();
-//     });
-//   });
-  
-//   it('does a thing', function() {
-//     return someAsyncFunction().then(function (result) {
-//       expect(result).toEqual(someExpectedValue);
-//     });
-//   });
-
-// });
-
-
 describe("Testing async functions", function() {
   
   beforeEach(async function() {
     await delayExecution();
   });
   
-  it('it should delay for few minutes', async function() {
+  it('Give the palettes time to load', async function() {
     let completed = false;
     completed  = await delayExecution();
     expect(completed).toBeTruthy();
