69fef7e2182ce02690f10f83a1b9d31bf17ea201
Favour Kelvin <favourkelvin17@gmail.com>
2019-07-09 03:35:04 +0100

    minified the css file

--- gulpfile.js
+++ gulpfile.js
@@ -11,26 +11,40 @@ const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 const replace = require('gulp-replace');
+const minifyCSS = require("gulp-minify-css");
+const cleanCSS = require('gulp-clean-css');
+const gulp = require('gulp'); 
 
 
 // File paths
 const files = { 
     // scssPath: 'css/**/*.sass',
-    jsPath: 'js/**/*.js'
+    jsPath: 'js/**/*.js',
+    cssPath: 'css/*.css',
+    sassPath: 'css/*.sass'
+
 }
 
-// // Sass task: compiles the style.scss file into style.css
-// function scssTask(){    
-//     return src(files.scssPath)
-//         .pipe(sourcemaps.init()) // initialize sourcemaps first
-//         .pipe(sass()) // compile SCSS to CSS
-//         .pipe(postcss([ autoprefixer(), cssnano() ])) // PostCSS plugins
-//         .pipe(sourcemaps.write('.')) // write sourcemaps file in current directory
-//         .pipe(dest('dist')
-//     ); // put final CSS in dist folder
-// }
+function sassTask(){    
+    return src(files.sassPath)
+        .pipe(sourcemaps.init()) // initialize sourcemaps first
+        .pipe(sass()) // compile SCSS to CSS
+        .pipe(postcss([ autoprefixer(), cssnano() ])) // PostCSS plugins
+        .pipe(sourcemaps.write('.')) // write sourcemaps file in current directory
+        .pipe(dest('dist/css')
+    ); // put final CSS in dist folder
+}
 
-// JS task: concatenates and uglifies JS files to script.js
+function cssTask () { 
+    return src(files.cssPath)
+               .pipe(minifyCSS({compatibility: 'ie8'}))
+               .pipe(gulp.dest('dist/css'));
+};
+
+
+
+
+// JS task: concatenates and uglifies JS files to app.min.js
 function jsTask(){
     return src([
         files.jsPath
@@ -57,15 +71,15 @@ function cacheBustTask(){
 // Watch task: watch SCSS and JS files for changes
 // If any change, run scss and js tasks simultaneously
 function watchTask(){
-    watch([ files.jsPath ], 
-        parallel( jsTask ));    
+    watch([ files.jsPath, files.cssPath, files.sassPath ], 
+        parallel( jsTask, cssTask, sassTask));    
 }
 
 // Export the default Gulp task so it can be run
 // Runs the scss and js tasks simultaneously
 // then runs cacheBust, then watch task
 exports.default = series(
-    parallel( jsTask ), 
+    parallel( jsTask, cssTask , sassTask ), 
     cacheBustTask,
     watchTask
 );
