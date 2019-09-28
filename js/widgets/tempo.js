1d3abb83c3a5463815030c79ac2d741578079929
Walter Bender <walter.bender@gmail.com>
2019-09-28 17:38:19 -0400

    don't allow 0 BPM

--- js/widgets/tempo.js
+++ js/widgets/tempo.js
@@ -375,7 +375,7 @@ function Tempo () {
         for (var i = 0; i < this.BPMs.length; i++) {
             this._directions.push(1);
             this._widgetFirstTimes.push(this._logo.firstNoteTime);
-            if (this.BPMs[i] < 0) {
+            if (this.BPMs[i] <= 0) {
                 this.BPMs[i] = 30;
             }
 
