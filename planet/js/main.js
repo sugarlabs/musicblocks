d73596fe2c291fb106bfef054e9a68865d45b077
Samyok Nepal <nepalsamyok@gmail.com>
2019-12-08 09:23:30 -0600

    Added recheck in case onPlanetLoad is not a function

--- planet/js/main.js
+++ planet/js/main.js
@@ -4,6 +4,13 @@ window.makePlanet = function(isMusicBlocks,storage,translationFunction) {
     window._=translationFunction;
     window.p = new Planet(isMusicBlocks,storage);
     window.p.init().then(()=>{
-        parent.onPlanetLoad();
+        let retryOnPlanetLoad = setInterval(()=>{
+            if(parent.onPlanetLoad){
+                parent.onPlanetLoad();
+                clearInterval(retryOnPlanetLoad);
+            } else {
+                console.log("Project not ready to load yet, retrying in 1 second...");
+            }
+        }, 1000);    
     });
 };
