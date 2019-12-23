6e7e762f57a265919a3613827655396e6cd14e67
Marcus Chong <marcus.jn.chong@gmail.com>
2019-12-22 19:30:12 -0500

    change file extension

--- js/SaveInterface.js
+++ js/SaveInterface.js
@@ -359,7 +359,7 @@ function SaveInterface(PlanetInterface) {
         var data = saveMxmlOutput(this.logo);
         console.log("data is:");
         console.log(data);
-        this.download('musicxml', 'data:text;utf8,' + encodeURIComponent(data), filename);
+        this.download('xml', 'data:text;utf8,' + encodeURIComponent(data), filename);
         this.logo.runningMxml = false;
     }
 
