a2cad37ce889d0f4a1882011be01105b0f24add5
aviral243 <aviralgangwar24@gmail.com>
2020-02-25 03:30:46 +0530

    implemented browser check

--- js/activity.js
+++ js/activity.js
@@ -338,6 +338,43 @@ function Activity() {
         toolbarHeight = document.getElementById("toolbars").offsetHeight;
     };
 
+    // Checks which browser is MB running from
+
+    this.doBrowserCheck = function() {
+        var matched, browser;
+        jQuery.uaMatch = function( ua ) {
+            ua = ua.toLowerCase();
+
+            var match = /(chrome)[ \/]([\w.]+)/.exec( ua ) ||
+                /(webkit)[ \/]([\w.]+)/.exec( ua ) ||
+                /(opera)(?:.*version|)[ \/]([\w.]+)/.exec( ua ) ||
+                /(msie) ([\w.]+)/.exec( ua ) ||
+                ua.indexOf("compatible") < 0 && /(mozilla)(?:.*? rv:([\w.]+)|)/.exec( ua ) ||
+                [];
+
+            return {
+                browser: match[ 1 ] || "",
+                version: match[ 2 ] || "0"
+            };
+        };
+
+        matched = jQuery.uaMatch( navigator.userAgent );
+        browser = {};
+
+        if ( matched.browser ) {
+            browser[ matched.browser ] = true;
+            browser.version = matched.version;
+        }
+
+        if ( browser.chrome ) {
+            browser.webkit = true;
+        } else if ( browser.webkit ) {
+            browser.safari = true;
+        }
+
+        jQuery.browser = browser;
+    }
+
     /*
      * Sets up right click functionality opening the context menus
      * (if block is right clicked)
