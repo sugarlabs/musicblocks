a0e3434a2bcbaa9dd984f3c525a29c8426707dc5
Ishakikani9117 <ishakikani999@gmail.com>
2020-03-12 20:23:07 +0530

    Revert "added space"
    This reverts commit 4e08fc7091727fb1ed072909876aa6da5d16ca6b.

--- js/lilypond.js
+++ js/lilypond.js
@@ -640,7 +640,6 @@ processLilypondNotes = function(lilypond, logo, turtle) {
                     }
 
                 }
-                logo.notationNotes[turtle] += " ";
 
                 if (obj[NOTATIONSTACCATO]) {
                     logo.notationNotes[turtle] += "\\staccato";
