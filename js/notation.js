c508a29dee754b30ab0087bc765cd30626fe64f9
Anindya Kundu <anindya.k22@outlook.com>
2020-06-17 16:36:25 +0530

    Add documentation

--- js/notation.js
+++ js/notation.js
@@ -1,4 +1,33 @@
+/**
+ * @file This contains the encapsulation related to notations (used for lilypond, abc, etc/).
+ * @author Walter Bender
+ *
+ * @copyright 2014-2020 Walter Bender
+ * @copyright 2020 Anindya Kundu
+ *
+ * @license
+ * This program is free software; you can redistribute it and/or modify it under the terms of
+ * the The GNU Affero General Public License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with this
+ * library; if not, write to the Free Software Foundation, 51 Franklin Street, Suite 500 Boston,
+ * MA 02110-1335 USA.
+ */
+
+/**
+ * Class for managing notations (used for lilypond, abc, etc).
+ *
+ * @class
+ * @classdesc This is the prototype that encapsulates the string arrays (in JSON format) that
+ * store relevant information about the notations for exporting to lilypond, abc, etc. Also
+ * contains the methods to modify them.
+ */
 class Notation {
+    /**
+     * @constructor
+     * @param {Object} logo - object of Logo
+     */
     constructor(logo) {
         this._logo = logo;
 
