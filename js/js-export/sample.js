98f4f6e42c31d3221f24c35b1193578352a7a298
Anindya Kundu <anindya.k22@outlook.com>
2020-08-11 23:13:21 +0530

    Add info

--- js/js-export/sample.js
+++ js/js-export/sample.js
@@ -1,3 +1,19 @@
+/**
+ * @file This contains the starter code and API for the JavaScript Editor Widget.
+ * @author Anindya Kundu
+ *
+ * @copyright 2020 Anindya Kundu
+ *
+ * @license
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * The GNU Affero General Public License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with this
+ * library; if not, write to the Free Software Foundation, 51 Franklin Street, Suite 500 Boston,
+ * MA 02110-1335 USA.
+*/
+
 const JS_API =
 `/*
 ============================================================
