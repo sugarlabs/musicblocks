b0ca482828ab517e4f5ff14fd4dd6916d1e30c42
Shubham Jain <jshubham94110@gmail.com>
2020-12-11 17:22:04 +0530

    const removal

--- js/widgets/statistics.js
+++ js/widgets/statistics.js
@@ -10,7 +10,7 @@
 // Foundation, 51 Franklin Street, Suite 500 Boston, MA 02110-1335 USA
 
 const BUTTONSIZE = 53;
-const ICONSIZE = 32;
+// const ICONSIZE = 32;
 
 /**
  * This widget makes displays the status of selected parameters and notes as they are being played.
