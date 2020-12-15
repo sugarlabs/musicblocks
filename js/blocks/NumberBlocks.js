eff1ab66941b147e1ac29e76fca8f340db8d9fc5
ricknjacky <jaikishanbrijwani@gmail.com>
2020-12-15 22:20:36 +0530

    undo changes for japan

--- js/blocks/NumberBlocks.js
+++ js/blocks/NumberBlocks.js
@@ -329,7 +329,7 @@ function setupNumberBlocks() {
 
             this.fontsize = 9;
             this.formBlock({
-                name: this.lang === "ja" ? "/" : "/",
+                name: this.lang === "ja" ? "➗" : "/",
                 args: 2,
                 defaults: [1, 4]
             });
