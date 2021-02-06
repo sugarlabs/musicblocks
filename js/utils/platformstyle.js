19b761e9ac98a26901b871616e08994878389ccd
Daksh Doshi <ddaksh.2711@gmail.com>
2021-02-06 19:04:32 +0530

    Add platform to global

--- js/utils/platformstyle.js
+++ js/utils/platformstyle.js
@@ -15,7 +15,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-/*global showMaterialHighlight */
+/*global showMaterialHighlight,platform */
 
 window.platform = {
     android: /Android/i.test(navigator.userAgent),
