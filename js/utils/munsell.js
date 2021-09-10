938378bc2e1cc4e4ccc3ed155cf15429ed330581
Walter Bender <walter@sorcero.com>
2021-09-09 20:35:01 -0400

    fix range of chroma

--- js/utils/munsell.js
+++ js/utils/munsell.js
@@ -6797,7 +6797,7 @@ function getcolor(color) {
 
     const v = Math.floor(COLORS40[h1][0] * p + COLORS40[h2][0] * (1 - p));
     const c = Math.floor(COLORS40[h1][1] * p + COLORS40[h2][1] * (1 - p));
-    return [v * 10, (c * 100) / 14, interpColor(COLORS40[h1][2], COLORS40[h2][2], p)];
+    return [v * 10, c * 100 / 28, interpColor(COLORS40[h1][2], COLORS40[h2][2], p)];
 }
 
 /**
