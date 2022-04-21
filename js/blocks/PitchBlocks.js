9d7e1a7dcf8817d75893491c502ba8d9f2a1ca84
Agatha Ta-Goetz <90356606+agathata@users.noreply.github.com>
2022-04-21 11:31:47 -0400

    add percent symbol to pitch offsets in custom temperaments for user clarity (#3030)

--- js/blocks/PitchBlocks.js
+++ js/blocks/PitchBlocks.js
@@ -1097,7 +1097,7 @@ function setupPitchBlocks(activity) {
             this.setPalette("pitch", activity);
             this.makeMacro((x, y) => [
                 [0, "pitch", x, y, [null, 1, 2, null]],
-                [1, ["customNote", { value: "C(+0)" }], 0, 0, [0]],
+                [1, ["customNote", { value: "C(+0%)" }], 0, 0, [0]],
                 [2, ["number", { value: 4 }], 0, 0, [0]]
             ]);
             this.hidden = true;
