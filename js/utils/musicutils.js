551f3242a46b1a6c127e010b90a7318c8026fd35
Ashutosh Gautam <75945709+Traitor000@users.noreply.github.com>
2022-08-04 00:55:36 +0530

    Regression Resolved (#3054)
    * Regression Resolved
    
    * Comments reviewed and made necessary changes
    
    * Used Prettier to format a section of code

--- js/utils/musicutils.js
+++ js/utils/musicutils.js
@@ -3228,8 +3228,7 @@ function _calculate_pitch_number(activity, np, tur) {
                 // Hertz
                 obj = frequencyToPitch(np);
             }
-
-        } catch(e) {
+        } catch (e) {
             activity.errorMsg(INVALIDPITCH);
             obj = ["G", 4];
         }
@@ -3331,7 +3330,32 @@ function _getStepSize(keySignature, pitch, direction, transposition, temperament
         thisPitch = STOSHARP[thisPitch];
     }
 
-    let ii = scale.indexOf(thisPitch);
+    function logicalEquals(s1, s2) {
+        // console.debug(s1,s2);
+        if (s1 == s2) {
+            return true;
+        } else if (s1 == "E♯" && s2 == "F") {
+            return true;
+        } else if (s1 == "E" && s2 == "F♭") {
+            return true;
+        } else if (s1 == "F" && s2 == "E♯") {
+            return true;
+        } else if (s1 == "F♭" && s2 == "E") {
+            return true;
+        } else if (s1 == "B♯" && s2 == "C") {
+            return true;
+        } else if (s1 == "B" && s2 == "C♭") {
+            return true;
+        } else if (s1 == "C" && s2 == "B♯") {
+            return true;
+        } else if (s1 == "C♭" && s2 == "B") {
+            return true;
+        }
+        return false;
+    }
+
+    let ii = scale.findIndex((scale) => logicalEquals(scale, pitch)); //indexOf() replaced by findIndex()
+    // let ii = scale.indexOf(thisPitch);
     if (ii !== -1) {
         if (direction === "up") {
             return halfSteps[ii];
