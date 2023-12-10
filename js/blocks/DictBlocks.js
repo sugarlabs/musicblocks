229948a016cd84a482d2352511514bf9adfbf25f
Karan-Palan <karanpalan007@gmail.com>
2023-12-10 19:57:48 +0530

    Added JSdoc style to these 4 files

--- js/blocks/DictBlocks.js
+++ js/blocks/DictBlocks.js
@@ -18,21 +18,62 @@
 /* exported setupDictBlocks */
 
 function setupDictBlocks(activity) {
+    /**
+     * Represents a block used to display the contents of a dictionary at the top of the screen.
+     * @extends {FlowBlock}
+     */
     class ShowDictBlock extends FlowBlock {
+        /**
+         * Constructs a ShowDictBlock.
+         */
         constructor() {
             super("showDict");
-            this.setPalette("dictionary", activity);
-            this.beginnerBlock(true);
-            this.hidden = this.deprecated = true;
 
+            /**
+             * Sets the palette for the block.
+             * @param {string} "dictionary" - The palette category.
+             * @param {Activity} activity - The activity associated with the block.
+             */
+            this.setPalette("dictionary", activity);
+
+            /**
+             * Indicates whether the block is a beginner block.
+             * @type {boolean}
+             */
+            this.beginnerBlock(true);
+
+            /**
+             * Indicates whether the block is hidden.
+             * @type {boolean}
+             */
+            this.hidden = true;
+
+            /**
+             * Indicates whether the block is deprecated.
+             * @type {boolean}
+             */
+            this.deprecated = true;
+
+            /**
+             * Sets the help string for the block.
+             * @type {string[]}
+             */
             this.setHelpString([
-                _("The Show-dictionary block displays the contents of the dictionary at the top of the screen."),
+                _(
+                    "The Show-dictionary block displays the contents of the dictionary at the top of the screen."
+                ),
                 "documentation",
                 ""
             ]);
 
+            /**
+             * Forms the block with specified configuration.
+             * @param {Object} config - The configuration object.
+             * @param {string} config.name - The name of the block.
+             * @param {string} config.args - The argument for the block.
+             * @param {string[]} config.defaults - The default values for the arguments.
+             */
             this.formBlock({
-                //.TRANS: Display the dictionary contents
                 name: _("show dictionary"),
                 args: 1,
                 argTypes: ["anyin"],
@@ -40,6 +81,13 @@ function setupDictBlocks(activity) {
             });
         }
 
+        /**
+         * Handles the flow of the block.
+         * @param {Array} args - The arguments passed to the block.
+         * @param {Logo} logo - The Logo instance.
+         * @param {Turtle} turtle - The Turtle instance.
+         * @param {string} blk - The block identifier.
+         */
         flow(args, logo, turtle, blk) {
             if (args[0] === null) {
                 activity.errorMsg(NOINPUTERRORMSG, blk);
@@ -49,19 +97,47 @@ function setupDictBlocks(activity) {
             Turtle.DictActions.showDict(args[0], turtle);
         }
     }
-
+    /**
+     * Represents a block used to return a dictionary.
+     * @extends {LeftBlock}
+     */
     class DictBlock extends LeftBlock {
+        /**
+         * Constructs a DictBlock.
+         */
         constructor() {
             super("dictionary");
+
+            /**
+             * Sets the palette for the block.
+             * @param {string} "dictionary" - The palette category.
+             * @param {Activity} activity - The activity associated with the block.
+             */
             this.setPalette("dictionary", activity);
+
+            /**
+             * Indicates whether the block is a beginner block.
+             * @type {boolean}
+             */
             this.beginnerBlock(true);
 
+            /**
+             * Sets the help string for the block.
+             * @type {string[]}
+             */
             this.setHelpString([
                 _("The Dictionary block returns a dictionary."),
                 "documentation",
                 ""
             ]);
 
+            /**
+             * Forms the block with specified configuration.
+             * @param {Object} config - The configuration object.
+             * @param {string} config.name - The name of the block.
+             * @param {string} config.args - The argument for the block.
+             * @param {string[]} config.defaults - The default values for the arguments.
+             */
             this.formBlock({
                 name: _("dictionary"),
                 args: 1,
@@ -70,6 +146,14 @@ function setupDictBlocks(activity) {
             });
         }
 
+        /**
+         * Handles the argument of the block.
+         * @param {Logo} logo - The Logo instance.
+         * @param {Turtle} turtle - The Turtle instance.
+         * @param {string} blk - The block identifier.
+         * @param {string} receivedArg - The received argument.
+         * @returns {object} The dictionary obtained from the specified argument.
+         */
         arg(logo, turtle, blk, receivedArg) {
             const cblk = activity.blocks.blockList[blk].connections[1];
             if (cblk === null) {
@@ -81,22 +165,50 @@ function setupDictBlocks(activity) {
             return Turtle.DictActions.getDict(a, turtle);
         }
     }
-
+    /**
+     * Represents a block used to return a value in the dictionary for a specified key.
+     * @extends {LeftBlock}
+     */
     class GetDictBlock extends LeftBlock {
+        /**
+         * Constructs a GetDictBlock.
+         */
         constructor() {
             super("getDict");
+
+            /**
+             * Sets the palette for the block.
+             * @param {string} "dictionary" - The palette category.
+             * @param {Activity} activity - The activity associated with the block.
+             */
             this.setPalette("dictionary", activity);
+
+            /**
+             * Indicates whether the block is a beginner block.
+             * @type {boolean}
+             */
             this.beginnerBlock(true);
 
+            /**
+             * Sets the help string for the block.
+             * @type {string[]}
+             */
             this.setHelpString([
                 _("The Get-dict block returns a value in the dictionary for a specified key."),
                 "documentation",
                 ""
             ]);
 
-            // labels: [this.lang === "js" ? _("do2") : _("do")];
+            /**
+             * Forms the block with specified configuration.
+             * @param {Object} config - The configuration object.
+             * @param {string} config.name - The name of the block.
+             * @param {string} config.args - The argument for the block.
+             * @param {string[]} config.argTypes - The types of arguments for the block.
+             * @param {string[]} config.argLabels - The labels for the arguments.
+             * @param {string[]} config.defaults - The default values for the arguments.
+             */
             this.formBlock({
-                //.TRANS: retrieve a value from the dictionary with a given key
                 name: _("get value"),
                 args: 2,
                 argTypes: ["anyin", "anyin"],
@@ -105,6 +217,14 @@ function setupDictBlocks(activity) {
             });
         }
 
+        /**
+         * Handles the argument of the block.
+         * @param {Logo} logo - The Logo instance.
+         * @param {Turtle} turtle - The Turtle instance.
+         * @param {string} blk - The block identifier.
+         * @param {string} receivedArg - The received argument.
+         * @returns {object} The value obtained from the specified dictionary and key.
+         */
         arg(logo, turtle, blk, receivedArg) {
             const cblk1 = activity.blocks.blockList[blk].connections[1];
             const cblk2 = activity.blocks.blockList[blk].connections[2];
@@ -119,21 +239,50 @@ function setupDictBlocks(activity) {
             return Turtle.DictActions.getValue(a, k, turtle, blk);
         }
     }
-
+    /**
+     * Represents a block used to set a value in the dictionary for a specified key.
+     * @extends {FlowBlock}
+     */
     class SetDictBlock extends FlowBlock {
+        /**
+         * Constructs a SetDictBlock.
+         */
         constructor() {
             super("setDict");
+
+            /**
+             * Sets the palette for the block.
+             * @param {string} "dictionary" - The palette category.
+             * @param {Activity} activity - The activity associated with the block.
+             */
             this.setPalette("dictionary", activity);
+
+            /**
+             * Indicates whether the block is a beginner block.
+             * @type {boolean}
+             */
             this.beginnerBlock(true);
 
+            /**
+             * Sets the help string for the block.
+             * @type {string[]}
+             */
             this.setHelpString([
                 _("The Set-dict block sets a value in the dictionary for a specified key."),
                 "documentation",
                 ""
             ]);
 
+            /**
+             * Forms the block with specified configuration.
+             * @param {Object} config - The configuration object.
+             * @param {string} config.name - The name of the block.
+             * @param {string} config.args - The argument for the block.
+             * @param {string[]} config.argTypes - The types of arguments for the block.
+             * @param {string[]} config.argLabels - The labels for the arguments.
+             * @param {string[]} config.defaults - The default values for the arguments.
+             */
             this.formBlock({
-                //.TRANS: set a value in the dictionary for a given key
                 name: _("set value"),
                 args: 3,
                 argTypes: ["anyin", "anyin", "anyin"],
@@ -142,6 +291,13 @@ function setupDictBlocks(activity) {
             });
         }
 
+        /**
+         * Handles the flow of the block.
+         * @param {Array} args - The arguments for the block.
+         * @param {Logo} logo - The Logo instance.
+         * @param {Turtle} turtle - The Turtle instance.
+         * @param {string} blk - The block identifier.
+         */
         flow(args, logo, turtle, blk) {
             if (args[0] === null || args[1] === null || args[2] === null) {
                 activity.errorMsg(NOINPUTERRORMSG, blk);
@@ -151,19 +307,48 @@ function setupDictBlocks(activity) {
             Turtle.DictActions.setValue(...args, turtle);
         }
     }
-
+    /**
+     * Represents a block used to return a value in the dictionary for a specified key.
+     * @extends {LeftBlock}
+     */
     class GetDictBlock2 extends LeftBlock {
+        /**
+         * Constructs a GetDictBlock2.
+         */
         constructor() {
             super("getDict2");
+
+            /**
+             * Sets the palette for the block.
+             * @param {string} "dictionary" - The palette category.
+             * @param {Activity} activity - The activity associated with the block.
+             */
             this.setPalette("dictionary", activity);
+
+            /**
+             * Indicates whether the block is a beginner block.
+             * @type {boolean}
+             */
             this.beginnerBlock(true);
 
+            /**
+             * Sets the help string for the block.
+             * @type {string[]}
+             */
             this.setHelpString([
                 _("The Get-dict block returns a value in the dictionary for a specified key."),
                 "documentation",
                 ""
             ]);
 
+            /**
+             * Forms the block with specified configuration.
+             * @param {Object} config - The configuration object.
+             * @param {string} config.name - The name of the block.
+             * @param {string} config.args - The argument for the block.
+             * @param {string[]} config.argTypes - The types of arguments for the block.
+             * @param {string[]} config.defaults - The default values for the arguments.
+             */
             this.formBlock({
                 //.TRANS: retrieve a value from the dictionary with a given key
                 name: _("get value"),
@@ -173,6 +358,14 @@ function setupDictBlocks(activity) {
             });
         }
 
+        /**
+         * Gets the value from the dictionary for a specified key.
+         * @param {Logo} logo - The Logo instance.
+         * @param {Turtle} turtle - The Turtle instance.
+         * @param {string} blk - The block identifier.
+         * @param {Object} receivedArg - The received argument.
+         * @returns {*} The value from the dictionary.
+         */
         arg(logo, turtle, blk, receivedArg) {
             const cblk1 = activity.blocks.blockList[blk].connections[1];
             if (cblk1 === null) {
@@ -186,19 +379,49 @@ function setupDictBlocks(activity) {
             return Turtle.DictActions.getValue(a, k, turtle, blk);
         }
     }
-
+    /**
+     * Represents a block used to set a value in the dictionary for a specified key.
+     * @extends {FlowBlock}
+     */
     class SetDictBlock2 extends FlowBlock {
+        /**
+         * Constructs a SetDictBlock2.
+         */
         constructor() {
             super("setDict2");
+
+            /**
+             * Sets the palette for the block.
+             * @param {string} "dictionary" - The palette category.
+             * @param {Activity} activity - The activity associated with the block.
+             */
             this.setPalette("dictionary", activity);
+
+            /**
+             * Indicates whether the block is a beginner block.
+             * @type {boolean}
+             */
             this.beginnerBlock(true);
 
+            /**
+             * Sets the help string for the block.
+             * @type {string[]}
+             */
             this.setHelpString([
                 _("The Set-dict block sets a value in the dictionary for a specified key."),
                 "documentation",
                 ""
             ]);
 
+            /**
+             * Forms the block with specified configuration.
+             * @param {Object} config - The configuration object.
+             * @param {string} config.name - The name of the block.
+             * @param {string} config.args - The argument for the block.
+             * @param {string[]} config.argTypes - The types of arguments for the block.
+             * @param {string[]} config.argLabels - The labels for the arguments.
+             * @param {string[]} config.defaults - The default values for the arguments.
+             */
             this.formBlock({
                 //.TRANS: set a value in the dictionary for a given key
                 name: _("set value"),
@@ -209,15 +432,20 @@ function setupDictBlocks(activity) {
             });
         }
 
+        /**
+         * Sets the value in the dictionary for a specified key.
+         * @param {Array} args - The arguments for the block.
+         * @param {Logo} logo - The Logo instance.
+         * @param {Turtle} turtle - The Turtle instance.
+         * @param {string} blk - The block identifier.
+         */
         flow(args, logo, turtle, blk) {
             if (args[0] === null || args[1] === null) {
                 activity.errorMsg(NOINPUTERRORMSG, blk);
                 return;
             }
 
-            Turtle.DictActions.setValue(
-                activity.turtles.ithTurtle(turtle).name, ...args, turtle
-            );
+            Turtle.DictActions.setValue(activity.turtles.ithTurtle(turtle).name, ...args, turtle);
         }
     }
 
