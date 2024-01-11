b5e15011649a692fa69b76b7bc9e0810d625b6c3
Aditya Pachauri <103623274+AdityyaX@users.noreply.github.com>
2024-01-11 13:58:07 +0530

    license year changed

--- js/loader.js
+++ js/loader.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2015 Yash Khandelwal
+// Copyright (c) 2024 Yash Khandelwal
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the The GNU Affero General Public
