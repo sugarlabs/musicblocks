828a641496cc23f67dd49ec38f67f06b9f454097
Ashish <ashishrout655@gmail.com>
2024-01-12 22:38:18 +0530

    limits pitch input value (#3594)
    * limits pitch input value
    
    * refactor error msg
    
    * limits minimum value from piemenu to 1 in pitch block

--- js/block.js
+++ js/block.js
@@ -4065,6 +4065,14 @@ class Block {
                 this.value = oldValue;
             }
             
+            if(this.blocks.blockList[cblk1].name === "pitch" && (this.value > 10 || this.value < 1)) {
+                const thisBlock = this.blocks.blockList.indexOf(this);
+                this.activity.errorMsg(_("Octave value must be between 1 and 10."), thisBlock);
+                this.activity.refreshCanvas();
+                this.label.value = oldValue;
+                this.value = oldValue;
+            }
+
             if(String(this.value).length > 12) {
                 const thisBlock = this.blocks.blockList.indexOf(this);
                 this.activity.errorMsg(_("Numbers can have at most 12 digits."), thisBlock);
