93c288990a8accc620a7ff11e7fdcf233bcad9eb
apsinghdev <ajeetpratap517@gmail.com>
2024-01-23 19:35:05 +0530

    fix phrasemaker

--- js/widgets/widgetWindows.js
+++ js/widgets/widgetWindows.js
@@ -194,8 +194,8 @@ class WidgetWindow {
 
         const disableScroll = () => {
             // Get the current page scroll position
-            const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-            const scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+            const scrollTop = window.scrollY || document.documentElement.scrollTop;
+            const scrollLeft = window.scrollX || document.documentElement.scrollLeft;
             // if any scroll is attempted,
             // set this to the previous value
             window.onscroll = () => {
@@ -482,7 +482,7 @@ class WidgetWindow {
         this._frame.style.width = "auto";
         this._frame.style.height = "auto";
         this._frame.style.minHeight = "420px";
-        this._frame.style.minWidth = "470px";
+        this._frame.style.minWidth = "700px";
     }
 
     /**
