adbc3989e786c2735701b7df939cd04c1febe9fd
apsinghdev <ajeetpratap517@gmail.com>
2024-07-01 16:37:27 +0530

    Fix input boxes

--- js/activity.js
+++ js/activity.js
@@ -5085,8 +5085,8 @@ class Activity {
             document.addEventListener(
                 "mousedown",
                 (event) => {
-                    event.preventDefault();
-                    event.stopPropagation();
+                    // event.preventDefault();
+                    // event.stopPropagation();
                     if (event.target.id === "myCanvas") {
                         this._createDrag(event);
                     }
