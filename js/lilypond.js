67e66a9c36f8d2a41f75adacaccf2763e655ccf9
Amrit Rai <iamamrit27@gmail.com>
2024-09-06 18:18:05 +0530

    used regex to shrink code

--- js/lilypond.js
+++ js/lilypond.js
@@ -66,21 +66,13 @@ const processLilypondNotes = (lilypond, logo, turtle) => {
             note = pitchObj[0] + pitchObj[1];
         }
 
-        return note
-            .replace(/♮/g, "!")
-            .replace(/♯/g, "is")
-            .replace(/♭/g, "es")
-            .replace(/10/g, "''''''''")
-            .replace(/1/g, ",, ")
-            .replace(/2/g, ", ")
-            .replace(/3/g, "")
-            .replace(/4/g, "'")
-            .replace(/5/g, "''")
-            .replace(/6/g, "'''")
-            .replace(/7/g, "''''")
-            .replace(/8/g, "''''''")
-            .replace(/9/g, "'''''''")
-            .toLowerCase();
+    const replacements = {
+            '♮': '!', '♯': 'is', '♭': 'es', '10': "''''''''",
+            '1': ',, ', '2': ', ', '3': '', '4': "'", '5': "''",
+            '6': "'''", '7': "''''", '8': "''''''", '9': "'''''''"
+        };
+
+        return note.replace(/[♮♯♭]|10|[1-9]/g, match => replacements[match]).toLowerCase();
     };
 
     let noteCounter = 0;
