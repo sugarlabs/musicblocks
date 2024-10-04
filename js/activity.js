bae54efac8bf7e8e8aa21511538bf4d60f5f68ff
dubeanant <147899880+dubeanant@users.noreply.github.com>
2024-10-04 19:51:17 +0530

    Fix search box hover behavior replacing input text (#4027)
    * Fix search box hover behavior replacing input text
    
    * Fix hover behavior and improve scrolling for search box

--- js/activity.js
+++ js/activity.js
@@ -2385,13 +2385,10 @@ class Activity {
                     that.doSearch();
                     if (event.keyCode === 13) this.searchWidget.style.visibility = "visible";
                 },
-                focus: (event, ui) => {
+                focus: (event) => {
                     event.preventDefault();
-                    that.searchWidget.value = ui.item.label;
                 }
             });
-
-            $j("#search").autocomplete("widget").addClass("scrollSearch");
 
             $j("#search").autocomplete("instance")._renderItem = (ul, item) => {
                 return $j("<li></li>")
@@ -2399,13 +2396,16 @@ class Activity {
                     .append(
                         '<img src="' +
                             item.artwork +
-                            '" height = "20px">' +
-                            "<a>" +
-                            " " +
+                            '" height="20px">' +
+                            "<a> " +
                             item.label +
                             "</a>"
                     )
-                    .appendTo(ul.css("z-index", 9999));
+                    .appendTo(ul.css({
+                        "z-index": 9999,
+                        "max-height": "200px",
+                        "overflow-y": "auto"
+                    }));
             };
             const searchInput = this.searchWidget.idInput_custom;
             if (!searchInput || searchInput.length <= 0) return;
