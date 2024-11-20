e9cfe9b84a070c31fae5567b9f36cf5bf13baaf2
Amrit <iamamrit27@gmail.com>
2024-11-20 08:57:38 +0530

    add jsdoc style documentation

--- js/js-export/API/PitchBlocksAPI.js
+++ js/js-export/API/PitchBlocksAPI.js
@@ -27,16 +27,33 @@
  * @classdesc methods are imported by a importMethod function call from MusicBlocks class.
  */
 class PitchBlocksAPI {
+    /**
+     * Plays a note with specified pitch and octave.
+     * @param {string} note - The note name to play (e.g., 'C', 'D', etc.)
+     * @param {number} octave - The octave number for the note
+     * @returns {Promise} Command execution promise
+     */
     playPitch(note, octave) {
         const args = JSInterface.validateArgs("playPitch", [note, octave]);
         return this.runCommand("playPitch", [args[0], args[1], 0, this.turIndex, MusicBlocks.BLK]);
     }
 
+    /**
+     * Steps the pitch up or down by a specified value.
+     * @param {number} value - The number of steps to move (positive for up, negative for down)
+     * @returns {Promise} Command execution promise
+     */
     stepPitch(value) {
         const args = JSInterface.validateArgs("stepPitch", [value]);
         return this.runCommand("stepPitch", [args[0], this.turIndex]);
     }
 
+    /**
+     * Plays the nth pitch in the current modal scale.
+     * @param {number} number - The scale degree to play
+     * @param {number} octave - The octave number
+     * @returns {Promise} Command execution promise
+     */
     playNthModalPitch(number, octave) {
         const args = JSInterface.validateArgs("playNthModalPitch", [number, octave]);
         return this.runCommand("playNthModalPitch", [
@@ -47,16 +64,32 @@ class PitchBlocksAPI {
         ]);
     }
 
+    /**
+     * Plays a pitch specified by its number value.
+     * @param {number} number - The pitch number to play
+     * @returns {Promise} Command execution promise
+     */
     playPitchNumber(number) {
         const args = JSInterface.validateArgs("playPitchNumber", [number]);
         return this.runCommand("playPitchNumber", [args[0], this.turIndex, MusicBlocks.BLK]);
     }
 
+    /**
+     * Plays a note at a specified frequency in Hertz.
+     * @param {number} value - The frequency in Hertz
+     * @returns {Promise} Command execution promise
+     */
     playHertz(value) {
         const args = JSInterface.validateArgs("playHertz", [value]);
         return this.runCommand("playHertz", [args[0], this.turIndex]);
     }
 
+    /**
+     * Sets an accidental (sharp, flat, etc.) for subsequent notes.
+     * @param {string} accidental - The accidental to apply
+     * @param {Function} flow - Flow function to execute after setting accidental
+     * @returns {string} ENDFLOWCOMMAND
+     */
     async setAccidental(accidental, flow) {
         const args = JSInterface.validateArgs("setAccidental", [accidental, flow]);
         await this.runCommand("setAccidental", [args[0], this.turIndex, MusicBlocks.BLK]);
@@ -64,6 +97,12 @@ class PitchBlocksAPI {
         return this.ENDFLOWCOMMAND;
     }
 
+    /**
+     * Transposes subsequent notes by a scalar value.
+     * @param {number} value - The number of scale degrees to transpose
+     * @param {Function} flow - Flow function to execute after transposition
+     * @returns {string} ENDFLOWCOMMAND
+     */
     async setScalarTranspose(value, flow) {
         const args = JSInterface.validateArgs("setScalarTranspose", [value, flow]);
         await this.runCommand("setScalarTranspose", [args[0], this.turIndex]);
@@ -71,6 +110,12 @@ class PitchBlocksAPI {
         return this.ENDFLOWCOMMAND;
     }
 
+    /**
+     * Transposes subsequent notes by a specified number of semitones.
+     * @param {number} value - The number of semitones to transpose
+     * @param {Function} flow - Flow function to execute after transposition
+     * @returns {string} ENDFLOWCOMMAND
+     */
     async setSemitoneTranspose(value, flow) {
         const args = JSInterface.validateArgs("setSemitoneTranspose", [value, flow]);
         await this.runCommand("setSemitoneTranspose", [args[0], this.turIndex]);
@@ -78,11 +123,24 @@ class PitchBlocksAPI {
         return this.ENDFLOWCOMMAND;
     }
 
+    /**
+     * Sets the register (octave range) for subsequent notes.
+     * @param {number} value - The register value to set
+     * @returns {Promise} Command execution promise
+     */
     setRegister(value) {
         const args = JSInterface.validateArgs("setRegister", [value]);
         return this.runCommand("setRegister", [args[0], this.turIndex]);
     }
 
+    /**
+     * Inverts the current pitch sequence based on specified parameters.
+     * @param {string} name - The name of the inversion
+     * @param {number} octave - The octave for the inversion
+     * @param {string} mode - The mode for the inversion
+     * @param {Function} flow - Flow function to execute after inversion
+     * @returns {string} ENDFLOWCOMMAND
+     */
     async invert(name, octave, mode, flow) {
         const args = JSInterface.validateArgs("invert", [name, octave, mode, flow]);
         await this.runCommand("invert", [args[0], args[1], args[2], this.turIndex]);
@@ -90,16 +148,32 @@ class PitchBlocksAPI {
         return this.ENDFLOWCOMMAND;
     }
 
+    /**
+     * Sets the pitch number offset for subsequent notes.
+     * @param {number} pitch - The pitch offset value
+     * @param {number} octave - The octave offset value
+     * @returns {Promise} Command execution promise
+     */
     setPitchNumberOffset(pitch, octave) {
         const args = JSInterface.validateArgs("setPitchNumberOffset", [pitch, octave]);
         return this.runCommand("setPitchNumberOffset", [args[0], args[1], this.turIndex]);
     }
 
+    /**
+     * Converts a number to its corresponding pitch name.
+     * @param {number} number - The number to convert
+     * @returns {string} The pitch name
+     */
     numToPitch(number) {
         const args = JSInterface.validateArgs("numToPitch", [number]);
         return Singer.PitchActions.numToPitch(args[0], "pitch", this.turIndex);
     }
 
+    /**
+     * Converts a number to its corresponding octave.
+     * @param {number} number - The number to convert
+     * @returns {number} The octave number
+     */
     numToOctave(number) {
         const args = JSInterface.validateArgs("numToOctave", [number]);
         return Singer.PitchActions.numToPitch(args[0], "octave", this.turIndex);
