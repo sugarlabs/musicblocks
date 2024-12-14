145fe8370e598160220ac7f93e5b1a5777070c49
omsuneri <142336291+omsuneri@users.noreply.github.com>
2024-12-14 22:31:20 +0530

    Update jest.setup.js

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,3 @@
 // A jest.setup.js file is used to run setup code before your tests are executed. 
+global.TextEncoder = require('util').TextEncoder;
+global.TextDecoder = require('util').TextDecoder;
