8be6d503616f11708f8b936a76d498622a3b79c8
Ajeet Pratap Singh <ajeetpratap517@gmail.com>
2024-12-20 03:17:51 +0530

    fix: add `jest` dependency and remove extra configs from `jest.config.js` (#4146)
    * fix/test: remove unnecessay configurations
    
    * chore: install and configure jest
    
    * chore: delete setup file as we don't need it initially

--- jest.setup.js
+++
@@ -1,3 +0,0 @@
-// A jest.setup.js file is used to run setup code before your tests are executed. 
-global.TextEncoder = require('util').TextEncoder;
-global.TextDecoder = require('util').TextDecoder;
