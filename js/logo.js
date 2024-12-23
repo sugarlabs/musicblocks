7e4c148677f0b6f12f8a0e7849b9f6dd209b3579
omsuneri <142336291+omsuneri@users.noreply.github.com>
2024-12-23 10:21:53 +0530

    Update logo.js
    Declaring `arg` within the function to avoid sharing state.

--- js/logo.js
+++ js/logo.js
@@ -1843,8 +1843,8 @@ class Logo {
 
         const suppressOutput = tur.singer.suppressOutput;
 
-        let arg;
         const __pen = (turtle, name, b, timeout) => {
+            let arg; // Declaring `arg` within the function to avoid sharing 
             switch (name) {
                 case "penup":
                 case "pendown":
