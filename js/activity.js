675702ab338269d74dd9d633adc58efc70e77ed4
Diwangshu Kakoty <142284646+Commanderk3@users.noreply.github.com>
2025-01-12 21:10:43 +0530

    record cancel works (#4260)
    * record cancel works
    
    * Translation function added
    
    * Click on stop sharing

--- js/activity.js
+++ js/activity.js
@@ -1282,8 +1282,16 @@ class Activity {
                     }
                 );
 
-                const filename = window.prompt("Enter file name"),
-                    downloadLink = document.createElement("a");
+                const filename = window.prompt(_("Enter file name"));
+                if (filename === null || filename.trim() === "") {
+                    alert(_("File save canceled"));
+                    flag = 0;
+                    recording();
+                    doRecordButton();
+                    return; // Exit without saving the file
+                }
+
+                const downloadLink = document.createElement("a");
                 downloadLink.href = URL.createObjectURL(blob);
                 downloadLink.download = `${filename}.webm`;
 
@@ -1295,7 +1303,7 @@ class Activity {
                 // eslint-disable-next-line no-use-before-define
                 recording();
                 doRecordButton();
-                that.textMsg("click on stop sharing");
+                that.textMsg(_("Click on stop sharing"));
             }
             /**
              * Stops the recording process.
