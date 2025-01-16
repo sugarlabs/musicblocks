a6f5608a4fdffcc5c4a8aef50c5a69631f957761
subhas-pramanik-09 <subhaspramanik864@gmail.com>
2025-01-16 09:32:09 +0530

    Creating view trash and more restore options

--- js/activity.js
+++ js/activity.js
@@ -3484,8 +3484,8 @@ class Activity {
                 }
                 trashView.classList.add('hidden');
             });
-            restoreLastIcon.setAttribute("title", _("Restore Last Item"));
-            restoreAllIcon.setAttribute("title", _("Restore All Items"));
+            restoreLastIcon.setAttribute("title", _("Restore last item"));
+            restoreAllIcon.setAttribute("title", _("Restore all items"));
         
             buttonContainer.appendChild(restoreLastIcon);
             buttonContainer.appendChild(restoreAllIcon);
