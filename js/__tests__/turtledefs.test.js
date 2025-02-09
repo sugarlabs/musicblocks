5b78d8241a9a9d179ffa5bfc72f99f173fc4e777
Om Santosh Suneri <142336291+omsuneri@users.noreply.github.com>
2025-02-10 03:53:41 +0530

    Updating test turtledefs.test.js

--- js/__tests__/turtledefs.test.js
+++ js/__tests__/turtledefs.test.js
@@ -5,24 +5,14 @@ const {
     LOGOJA1,
     NUMBERBLOCKDEFAULT,
     DEFAULTPALETTE,
-    getMainToolbarButtonNames,
-    getAuxToolbarButtonNames,
     TITLESTRING
 } = require('../turtledefs');
-
-jest.mock("../turtledefs", () => ({
-    ...jest.requireActual("../turtledefs"),
-    getMainToolbarButtonNames: jest.fn(() => ["RUNBUTTON", "STOPBUTTON"]),
-    getAuxToolbarButtonNames: jest.fn(() => ["HELPTURTLEBUTTON", "LANGUAGEBUTTON"])
-}));
 
 global.GUIDEURL = "guide url";
 global.RUNBUTTON = "RUNBUTTON";
 global.STOPBUTTON = "STOPBUTTON";
 global.HELPTURTLEBUTTON = "HELPTURTLEBUTTON";
 global.LANGUAGEBUTTON = "LANGUAGEBUTTON";
-global.getMainToolbarButtonNames = jest.fn(() => ["RUNBUTTON", "STOPBUTTON"]);
-global.getAuxToolbarButtonNames = jest.fn(() => ["HELPTURTLEBUTTON", "LANGUAGEBUTTON"]);
 
 if (GUIDEURL === "guide url") {
   GUIDEURL = "https://github.com/sugarlabs/turtleblocksjs/tree/master/guide/README.md"; 
@@ -45,22 +35,6 @@ describe("turtledefs.js", () => {
 
   test("GUIDEURL should default to the correct URL", () => {
     expect(GUIDEURL).toBe("https://github.com/sugarlabs/turtleblocksjs/tree/master/guide/README.md");
-  });
-
-  test("getMainToolbarButtonNames should return expected buttons", () => {
-    const buttons = getMainToolbarButtonNames();
-    expect(buttons).toBeDefined();
-    expect(Array.isArray(buttons)).toBe(true);
-    expect(buttons.length).toBeGreaterThan(0);
-    expect(buttons).toEqual(expect.arrayContaining([global.RUNBUTTON, global.STOPBUTTON]));
-  });
-
-  test("getAuxToolbarButtonNames should return expected auxiliary buttons", () => {
-    const buttons = getAuxToolbarButtonNames();
-    expect(buttons).toBeDefined();
-    expect(Array.isArray(buttons)).toBe(true);
-    expect(buttons.length).toBeGreaterThan(0);
-    expect(buttons).toEqual(expect.arrayContaining([global.HELPTURTLEBUTTON, global.LANGUAGEBUTTON]));
   });
 
   test("TITLESTRING should be defined", () => {
