c70d4cdaa9294f069d76bc4f85db06acd8eda440
Aman Chadha(IVIXMMI) <79802170+ac-mmi@users.noreply.github.com>
2025-02-21 09:58:58 +0530

    Add files via upload

--- js/utils/__tests__/tonemock.js
+++ js/utils/__tests__/tonemock.js
@@ -7,7 +7,7 @@ class Sampler {
     this.stop = jest.fn().mockReturnThis();
     this.triggerAttack = jest.fn().mockReturnThis();
     this.volume = {
-      value: 0, // Add this line
+      value: 0, 
       linearRampToValueAtTime: jest.fn().mockImplementation()
     }
     this.triggerRelease = jest.fn().mockReturnThis();
@@ -57,7 +57,6 @@ class Synth {
     this.synthOptions = synthOptions
     this.triggerAttackRelease = jest.fn().mockReturnThis();
     this.chain = jest.fn().mockReturnThis();
-    //new addition
     this.volume = {
       value: 0,
       linearRampToValueAtTime: jest.fn().mockImplementation()
@@ -83,7 +82,6 @@ class PolySynth {
     this.triggerAttack = jest.fn().mockReturnThis();
     this.start = jest.fn().mockReturnThis();
     this.triggerAttackRelease = jest.fn().mockReturnThis();
-    //new addition
     this.volume = {
       value: 0,
       linearRampToValueAtTime: jest.fn().mockImplementation()
