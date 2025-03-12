b54dd1892f1cb6685b12ee0fae1542d6eb858a28
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2025-03-12 23:49:23 +0600

    add license header

--- js/blocks/__tests__/BooleanBlocks.test.js
+++ js/blocks/__tests__/BooleanBlocks.test.js
@@ -1,3 +1,25 @@
+/**
+ * MusicBlocks v3.6.2
+ *
+ * @author Om Santosh Suneri
+ *
+ * @copyright 2025 Om Santosh Suneri
+ *
+ * @license
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 const { setupBooleanBlocks } = require('../BooleanBlocks');
 global._ = jest.fn((str) => str);
 global.BooleanBlock = jest.fn().mockImplementation((type) => ({
