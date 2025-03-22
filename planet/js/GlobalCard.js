ae82bd3d66a08968a7e5f809e4cf1efec23aeda4
omsuneri <omsuneri@gmail.com>
2025-03-22 15:14:46 +0530

    Resolving ESLint errors

--- planet/js/GlobalCard.js
+++ planet/js/GlobalCard.js
@@ -46,48 +46,48 @@ class GlobalCard {
                     <div class="card-action"> 
                         <div class="flexcontainer"> 
                             <a class="project-icon tooltipped" data-position="bottom" data-delay="50" data-tooltip="${_(
-                                "More Details"
-                            )}" id="global-project-more-details-{ID}"><i class="material-icons">info</i></a> 
+        "More Details"
+    )}" id="global-project-more-details-{ID}"><i class="material-icons">info</i></a> 
                             <a class="project-icon tooltipped" data-position="bottom" data-delay="50" data-tooltip="${_(
-                                "Open in Music Blocks"
-                            )}" id="global-project-open-{ID}"><i class="material-icons">launch</i></a> 
+        "Open in Music Blocks"
+    )}" id="global-project-open-{ID}"><i class="material-icons">launch</i></a> 
                             <a class="project-icon tooltipped" data-position="bottom" data-delay="50" data-tooltip="${_(
-                                "Merge with current project"
-                            )}" id="global-project-merge-{ID}"><i class="material-icons">merge_type</i></a> 
+        "Merge with current project"
+    )}" id="global-project-merge-{ID}"><i class="material-icons">merge_type</i></a> 
                             <a class="project-icon tooltipped" data-position="bottom" data-delay="50" data-tooltip="${`${
-                                Planet.ProjectStorage.isLiked(this.id) ? "Unlike" : "Like"
-                            } project`}"><i class="material-icons"id="global-like-icon-{ID}"></i><span class="likes-count" id="global-project-likes-{ID}"></span></a>
+        Planet.ProjectStorage.isLiked(this.id) ? "Unlike" : "Like"
+    } project`}"><i class="material-icons"id="global-like-icon-{ID}"></i><span class="likes-count" id="global-project-likes-{ID}"></span></a>
 
                             <div id="global-share-{ID}"> 
                                 <a class="project-icon tooltipped" data-position="bottom" data-delay="50" data-tooltip="${_(
-                                    "Share project"
-                                )}" id="global-project-share-{ID}"><i class="material-icons">share</i></a> 
+        "Share project"
+    )}" id="global-project-share-{ID}"><i class="material-icons">share</i></a> 
                             
                                 <div class="card share-card" id="global-sharebox-{ID}" style="display:none;"> 
                                     <div class="card-content shareurltext"> 
                                             <div class="shareurltitle">${_("Share")}</div> 
                                             <input type="text" name="shareurl" class="shareurlinput" data-originalurl="https://musicblocks.sugarlabs.org/index.html?id={ID}"> 
                                             <a class="copyshareurl tooltipped" onclick="copyURLToClipboard()" data-clipboard-text="https://musicblocks.sugarlabs.org/index.html?id={ID}&run=True" data-delay="50" data-tooltip="${_(
-                                                "Copy link to clipboard"
-                                            )}"><i class="material-icons"alt="Copy!">file_copy</i></a>
+        "Copy link to clipboard"
+    )}"><i class="material-icons"alt="Copy!">file_copy</i></a>
                                             <div class="shareurl-advanced" id="global-advanced-{ID}"> 
                                                     <div class="shareurltitle">${_("Flags")}</div> 
                                                     <div><input type="checkbox" name="run" id="global-checkboxrun-{ID}" checked><label for="global-checkboxrun-{ID}">${_(
-                                                        "Run project on startup."
-                                                    )}</label></div> 
+        "Run project on startup."
+    )}</label></div> 
                                                     <div><input type="checkbox" name="show" id="global-checkboxshow-{ID}"><label for="global-checkboxshow-{ID}">${_(
-                                                        "Show code blocks on startup."
-                                                    )}</label></div> 
+        "Show code blocks on startup."
+    )}</label></div> 
                                                     <div><input type="checkbox" name="collapse" id="global-checkboxcollapse-{ID}"><label for="global-checkboxcollapse-{ID}">${_(
-                                                        "Collapse code blocks on startup."
-                                                    )}</label></div> 
+        "Collapse code blocks on startup."
+    )}</label></div> 
                                             </div> 
                                     </div> 
 
                                     <div class="card-action"> 
                                         <a onclick="toggleExpandable('global-advanced-{ID}','shareurl-advanced');">${_(
-                                            "Advanced Options"
-                                        )}</a> 
+        "Advanced Options"
+    )}</a> 
                                     </div> 
                                 </div> 
                             </div> 
