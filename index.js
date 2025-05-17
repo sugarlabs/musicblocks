1961c8298bc44ce44cae601fb24f6d6cc0f806f8
KALLA GANASEKHAR <ganasekharkalla@gmail.com>
2025-05-18 03:23:17 +0530

    Updated delay timing

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname), {
     maxAge: "1h"
 }));
 
-const PORT = 3000;
+const PORT = 3005;
 app.listen(PORT, "127.0.0.1", () => {
     console.log(`Music Blocks running at http://127.0.0.1:${PORT}/`);
     console.log("Compression enabled");
