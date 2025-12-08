63fb1720ae2b7338015a460ab8d6fb7e0e6d0050
Harihara Vardhan <140606048+zealot-zew@users.noreply.github.com>
2025-12-08 22:57:49 +0530

    Collapsed Silence block resize Issue (#4833)
    * Collapsed Silence block resize Issue
    
    Increased the proto.extraWidth from 40 to 50 pixels to accommodate for the extra text caused by collapsing
    
    Testing:
    Visually tested with maximum values that can appear after collapsing
    
    * Fix block clipping after collapse by resetting EaselJS cache
    
    This PR fixes a rendering bug where collapsed blocks appear clipped until the user zooms or pans the canvas.
    
    * linting
    
    * lint parameters
    
    ---------
    
    Co-authored-by: Walter Bender <walter@sorceo.com>

--- js/block.js
+++ js/block.js
@@ -684,7 +684,7 @@ class Block {
                 this.disconnectedBitmap.visible = true;
             }
             this.container.updateCache();
-        };
+        }
     }
 
     /**
@@ -812,7 +812,7 @@ class Block {
             if (this.name === "interval") {
                 proto.extraWidth = 80;
             } else {
-                proto.extraWidth = 40;
+                proto.extraWidth = 50;
             }
             proto.zeroArgBlock();
             obj = proto.generator();
@@ -821,7 +821,7 @@ class Block {
         } else if (this.isCollapsible()) {
             proto = new ProtoBlock("collapse");
             proto.scale = this.protoblock.scale;
-            proto.extraWidth = 40;
+            proto.extraWidth = 50;
             proto.basicBlockCollapsed();
             obj = proto.generator();
             this.collapseArtwork = obj[0];
@@ -1427,13 +1427,13 @@ class Block {
                 if (this.name === "interval") {
                     proto.extraWidth = 80;
                 } else {
-                    proto.extraWidth = 40;
+                    proto.extraWidth = 50;
                 }
                 proto.zeroArgBlock();
             } else {
                 proto = new ProtoBlock("collapse");
                 proto.scale = this.protoblock.scale;
-                proto.extraWidth = 40;
+                proto.extraWidth = 50;
                 proto.basicBlockCollapsed();
             }
 
@@ -2205,8 +2205,6 @@ class Block {
             this.disconnectedHighlightBitmap.visible = false;
         }
 
-        this.updateCache();
-
         if (this.name === "action") {
             // Label the collapsed block with the action label.
             if (this.connections[1] !== null) {
@@ -2243,6 +2241,19 @@ class Block {
                 }
             }
         }
+        //Update visibility of standard vs highlight bitmaps first
+        this.unhighlight();
+
+        //Delete the old cache.
+        // We must delete it so the engine is forced to calculate the new size.
+        if (this.container.cacheID) {
+            this.container.uncache();
+        }
+
+        //Create a NEW cache.
+        this._createCache((that) => {
+            that.activity.refreshCanvas();
+        });
 
         this.updateCache();
         this.unhighlight();
