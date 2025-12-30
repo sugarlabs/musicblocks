d9062229a3962b5fd17acc2bbfc1e8f95c626138
AviraL0013 <aviralsapra11@gmail.com>
2025-12-31 02:14:25 +0530

    Add integration tests for PitchBlocks

--- js/blocks/PitchBlocks.js
+++ js/blocks/PitchBlocks.js
@@ -2112,3 +2112,7 @@ function setupPitchBlocks(activity) {
     new Pitch2Block().setup(activity);
     new PitchBlock().setup(activity);
 }
+
+if (typeof module !== "undefined" && module.exports) {
+  module.exports = { setupPitchBlocks };
+}
