430a03c64ce1e401b0671efba0dc0048b7b69fb9
Divyam Agarwal <iitdivyamagarwal@gmail.com>
2026-01-01 12:41:36 +0530

    Update tempo.test.js

--- js/widgets/tempo.test.js
+++ js/widgets/tempo.test.js
@@ -1,3 +1,25 @@
+/**
+ * MusicBlocks v3.6.2
+ *
+ * @author Divyam Agarwal
+ *
+ * @copyright 2026 Divyam Agarwal
+ *
+ * @license
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 const Tempo = require("./tempo.js");
 
 // --- 1. Global Mocks (Fake the Browser Environment) ---
