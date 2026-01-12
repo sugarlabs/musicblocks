2f9bc202ef408a94a1bed8712e0616cca9167580
DhyaniKavya <kavyadhyani222@gmail.com>
2026-01-12 20:42:44 +0530

    Fix ESLint/Prettier formatting in js/activity.js

--- js/activity.js
+++ js/activity.js
@@ -2022,7 +2022,7 @@ class Activity {
             const changeText = () => {
                 const randomLoadMessage =
                     messages.load_messages[
-                    Math.floor(Math.random() * messages.load_messages.length)
+                        Math.floor(Math.random() * messages.load_messages.length)
                     ];
                 document.getElementById("messageText").innerHTML = randomLoadMessage + "...";
                 counter++;
@@ -2963,11 +2963,11 @@ class Activity {
                     .data("item.autocomplete", item)
                     .append(
                         '<img src="' +
-                        item.artwork +
-                        '" height="20px">' +
-                        "<a> " +
-                        item.label +
-                        "</a>"
+                            item.artwork +
+                            '" height="20px">' +
+                            "<a> " +
+                            item.label +
+                            "</a>"
                     )
                     .appendTo(
                         ul.css({
@@ -3085,7 +3085,7 @@ class Activity {
                 if (
                     this.blocks.blockList[bottom].name === "hidden" &&
                     this.blocks.blockList[this.blocks.blockList[bottom].connections[0]].name ===
-                    "newnote"
+                        "newnote"
                 ) {
                     // The note block macro creates nine blocks.
                     const newlyCreatedBlock = this.blocks.blockList.length - 9;
@@ -4120,9 +4120,7 @@ class Activity {
 
             const topRightButtons = document.querySelectorAll("#buttoncontainerTOP .tooltipped");
             const gridElement = document.getElementById("Grid");
-            const btnY = gridElement
-                ? gridElement.getBoundingClientRect().top
-                : 70 + LEADING + 6;
+            const btnY = gridElement ? gridElement.getBoundingClientRect().top : 70 + LEADING + 6;
 
             this.changeTopButtonsPosition = value => {
                 topRightButtons.forEach(child => {
@@ -4494,8 +4492,8 @@ class Activity {
                         console.log(
                             "%cMusic Blocks",
                             "font-size: 24px; font-weight: bold; font-family: sans-serif; padding:20px 0 0 110px; background: url(" +
-                            imgUrl +
-                            ") no-repeat;"
+                                imgUrl +
+                                ") no-repeat;"
                         );
                         // eslint-disable-next-line no-console
                         console.log(
@@ -4568,10 +4566,10 @@ class Activity {
                 typeof flags !== "undefined"
                     ? flags
                     : {
-                        run: false,
-                        show: false,
-                        collapse: false
-                    };
+                          run: false,
+                          show: false,
+                          collapse: false
+                      };
             this.loading = true;
             document.body.style.cursor = "wait";
             this.doLoadAnimation();
@@ -4934,8 +4932,9 @@ class Activity {
                                 [
                                     "nameddo",
                                     {
-                                        value: `V: ${parseInt(lineId) + 1} Line ${staffBlocksMap[lineId]?.baseBlocks?.length + 1
-                                            }`
+                                        value: `V: ${parseInt(lineId) + 1} Line ${
+                                            staffBlocksMap[lineId]?.baseBlocks?.length + 1
+                                        }`
                                     }
                                 ],
                                 0,
@@ -4944,12 +4943,12 @@ class Activity {
                                     staffBlocksMap[lineId].baseBlocks.length === 0
                                         ? null
                                         : staffBlocksMap[lineId].baseBlocks[
-                                        staffBlocksMap[lineId].baseBlocks.length - 1
-                                        ][0][
-                                        staffBlocksMap[lineId].baseBlocks[
-                                            staffBlocksMap[lineId].baseBlocks.length - 1
-                                        ][0].length - 4
-                                        ][0],
+                                              staffBlocksMap[lineId].baseBlocks.length - 1
+                                          ][0][
+                                              staffBlocksMap[lineId].baseBlocks[
+                                                  staffBlocksMap[lineId].baseBlocks.length - 1
+                                              ][0].length - 4
+                                          ][0],
                                     null
                                 ]
                             ],
@@ -4965,8 +4964,9 @@ class Activity {
                                 [
                                     "text",
                                     {
-                                        value: `V: ${parseInt(lineId) + 1} Line ${staffBlocksMap[lineId]?.baseBlocks?.length + 1
-                                            }`
+                                        value: `V: ${parseInt(lineId) + 1} Line ${
+                                            staffBlocksMap[lineId]?.baseBlocks?.length + 1
+                                        }`
                                     }
                                 ],
                                 0,
@@ -5001,14 +5001,14 @@ class Activity {
                     staffBlocksMap[staffIndex].startBlock.length - 3
                 ][4][2] =
                     staffBlocksMap[staffIndex].baseBlocks[0][0][
-                    staffBlocksMap[staffIndex].baseBlocks[0][0].length - 4
+                        staffBlocksMap[staffIndex].baseBlocks[0][0].length - 4
                     ][0];
                 // Update the first namedo block with settimbre
                 staffBlocksMap[staffIndex].baseBlocks[0][0][
                     staffBlocksMap[staffIndex].baseBlocks[0][0].length - 4
                 ][4][0] =
                     staffBlocksMap[staffIndex].startBlock[
-                    staffBlocksMap[staffIndex].startBlock.length - 3
+                        staffBlocksMap[staffIndex].startBlock.length - 3
                     ][0];
                 const repeatblockids = staffBlocksMap[staffIndex].repeatArray;
                 for (const repeatId of repeatblockids) {
@@ -5020,7 +5020,7 @@ class Activity {
                             0,
                             [
                                 staffBlocksMap[staffIndex].startBlock[
-                                staffBlocksMap[staffIndex].startBlock.length - 3
+                                    staffBlocksMap[staffIndex].startBlock.length - 3
                                 ][0] /*setribmre*/,
                                 blockId + 1,
                                 staffBlocksMap[staffIndex].nameddoArray[staffIndex][0],
@@ -5029,8 +5029,8 @@ class Activity {
                                 ] === null
                                     ? null
                                     : staffBlocksMap[staffIndex].nameddoArray[staffIndex][
-                                    repeatId.end + 1
-                                    ]
+                                          repeatId.end + 1
+                                      ]
                             ]
                         ]);
                         staffBlocksMap[staffIndex].repeatBlock.push([
@@ -5064,7 +5064,7 @@ class Activity {
                             const secondnammedo = _searchIndexForMusicBlock(
                                 staffBlocksMap[staffIndex].baseBlocks[repeatId.end + 1][0],
                                 staffBlocksMap[staffIndex].nameddoArray[staffIndex][
-                                repeatId.end + 1
+                                    repeatId.end + 1
                                 ]
                             );
 
@@ -5088,13 +5088,13 @@ class Activity {
                         const prevnameddo = _searchIndexForMusicBlock(
                             staffBlocksMap[staffIndex].baseBlocks[repeatId.start - 1][0],
                             staffBlocksMap[staffIndex].baseBlocks[repeatId.start][0][
-                            currentnammeddo
+                                currentnammeddo
                             ][4][0]
                         );
                         const afternamedo = _searchIndexForMusicBlock(
                             staffBlocksMap[staffIndex].baseBlocks[repeatId.end][0],
                             staffBlocksMap[staffIndex].baseBlocks[repeatId.start][0][
-                            currentnammeddo
+                                currentnammeddo
                             ][4][1]
                         );
                         let prevrepeatnameddo = -1;
@@ -5102,17 +5102,17 @@ class Activity {
                             prevrepeatnameddo = _searchIndexForMusicBlock(
                                 staffBlocksMap[staffIndex].repeatBlock,
                                 staffBlocksMap[staffIndex].baseBlocks[repeatId.start][0][
-                                currentnammeddo
+                                    currentnammeddo
                                 ][4][0]
                             );
                         }
                         const prevBlockId =
                             staffBlocksMap[staffIndex].baseBlocks[repeatId.start][0][
-                            currentnammeddo
+                                currentnammeddo
                             ][4][0];
                         const currentBlockId =
                             staffBlocksMap[staffIndex].baseBlocks[repeatId.start][0][
-                            currentnammeddo
+                                currentnammeddo
                             ][0];
 
                         // Needs null checking optmizie
@@ -5126,7 +5126,7 @@ class Activity {
                             0,
                             [
                                 staffBlocksMap[staffIndex].baseBlocks[repeatId.start][0][
-                                currentnammeddo
+                                    currentnammeddo
                                 ][4][0],
                                 blockId + 1,
                                 currentBlockId,
@@ -5744,7 +5744,7 @@ class Activity {
             this.update = true;
         };
 
-        this.__showAltoAccidentals = () => { };
+        this.__showAltoAccidentals = () => {};
 
         /*
          * Shows musical alto staff
@@ -6411,12 +6411,12 @@ class Activity {
                     .data("item.autocomplete", item)
                     .append(
                         '<img src="' +
-                        item.artwork +
-                        '" height = "20px">' +
-                        "<a>" +
-                        " " +
-                        item.label +
-                        "</a>"
+                            item.artwork +
+                            '" height = "20px">' +
+                            "<a>" +
+                            " " +
+                            item.label +
+                            "</a>"
                     )
                     .appendTo(ul.css("z-index", 9999));
             };
@@ -6533,10 +6533,10 @@ class Activity {
             container.setAttribute(
                 "style",
                 "position: absolute; right:" +
-                (document.body.clientWidth - x) +
-                "px;  top: " +
-                y +
-                "px;"
+                    (document.body.clientWidth - x) +
+                    "px;  top: " +
+                    y +
+                    "px;"
             );
             document.getElementById("buttoncontainerBOTTOM").appendChild(container);
             return container;
