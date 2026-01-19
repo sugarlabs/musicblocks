6c5d951f2bf56206ec717cef6194cdb30ce8dba1
Kartik <kartiktripathi103@gmail.com>
2026-01-19 23:05:32 +0530

    chore: format js/turtle-singer.js with CI Prettier

--- js/turtle-singer.js
+++ js/turtle-singer.js
@@ -2060,8 +2060,9 @@ class Singer {
                         0,
                         null
                     );
-                    const pitchNumber = getTemperament(activity.logo.synth.inTemperament)
-                        .pitchNumber;
+                    const pitchNumber = getTemperament(
+                        activity.logo.synth.inTemperament
+                    ).pitchNumber;
                     const ratio = [];
                     const number = [];
                     const numerator = [];
