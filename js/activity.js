cb2f7da88fd769d2a0a93e986b3e2f057f667ba7
Rohit. <76153830+Rohit-rk07@users.noreply.github.com>
2026-01-26 01:02:21 +0530

    Fix Prevent duplicate window event listeners on activity reinitialisation (#5323)
    * fixed window focus issue on activity page
    
    * Ran prettier on js/activity.js

--- js/activity.js
+++ js/activity.js
@@ -866,7 +866,8 @@ class Activity {
         }
 
         //if any window resize event occurs:
-        this.addEventListener(window, "resize", () => repositionBlocks(this));
+        this._handleRepositionBlocksOnResize = () => repositionBlocks(this);
+        this.addEventListener(window, "resize", this._handleRepositionBlocksOnResize);
 
         /**
          * Finds and organizes blocks within the workspace.
@@ -1899,8 +1900,9 @@ class Activity {
                 // Queue and take first step.
                 if (!this.turtles.running()) {
                     this.logo.runLogoCommands();
-                    document.getElementById("stop").style.color =
-                        this.toolbar.stopIconColorWhenPlaying;
+                    document.getElementById(
+                        "stop"
+                    ).style.color = this.toolbar.stopIconColorWhenPlaying;
                 }
                 this.logo.step();
             } else {
@@ -2220,8 +2222,9 @@ class Activity {
                     i < this.palettes.dict[this.palettes.activePalette].protoList.length;
                     i++
                 ) {
-                    const name =
-                        this.palettes.dict[this.palettes.activePalette].protoList[i]["name"];
+                    const name = this.palettes.dict[this.palettes.activePalette].protoList[i][
+                        "name"
+                    ];
                     if (name in obj["FLOWPLUGINS"]) {
                         // eslint-disable-next-line no-console
                         console.log("deleting " + name);
@@ -3702,14 +3705,16 @@ class Activity {
         }
 
         let resizeTimeout;
-        this.addEventListener(window, "resize", () => {
+        this._handleWindowResize = () => {
             clearTimeout(resizeTimeout);
             resizeTimeout = setTimeout(() => {
                 handleResize();
                 this._setupPaletteMenu();
             }, 100);
-        });
-        this.addEventListener(window, "orientationchange", handleResize);
+        };
+        this.addEventListener(window, "resize", this._handleWindowResize);
+        this._handleOrientationChangeResize = handleResize;
+        this.addEventListener(window, "orientationchange", this._handleOrientationChangeResize);
         const that = this;
         const resizeCanvas_ = () => {
             try {
@@ -3722,7 +3727,12 @@ class Activity {
         };
 
         resizeCanvas_();
-        this.addEventListener(window, "orientationchange", resizeCanvas_);
+        this._handleOrientationChangeResizeCanvas = resizeCanvas_;
+        this.addEventListener(
+            window,
+            "orientationchange",
+            this._handleOrientationChangeResizeCanvas
+        );
 
         /*
          * Restore last stack pushed to trashStack back onto canvas.
@@ -4928,8 +4938,9 @@ class Activity {
                             }
                         }
                         staffBlocksMap[staffIndex].baseBlocks[0][0][firstnammedo][4][0] = blockId;
-                        staffBlocksMap[staffIndex].baseBlocks[repeatId.end][0][endnammedo][4][1] =
-                            null;
+                        staffBlocksMap[staffIndex].baseBlocks[repeatId.end][0][
+                            endnammedo
+                        ][4][1] = null;
 
                         blockId += 2;
                     } else {
@@ -4997,8 +5008,9 @@ class Activity {
                                 prevnameddo
                             ][4][1] = blockId;
                         } else {
-                            staffBlocksMap[staffIndex].repeatBlock[prevrepeatnameddo][4][3] =
-                                blockId;
+                            staffBlocksMap[staffIndex].repeatBlock[
+                                prevrepeatnameddo
+                            ][4][3] = blockId;
                         }
                         if (afternamedo !== -1) {
                             staffBlocksMap[staffIndex].baseBlocks[repeatId.end][0][
@@ -5849,8 +5861,8 @@ class Activity {
                                 let customName = "custom";
                                 if (myBlock.connections[1] !== null) {
                                     // eslint-disable-next-line max-len
-                                    customName =
-                                        this.blocks.blockList[myBlock.connections[1]].value;
+                                    customName = this.blocks.blockList[myBlock.connections[1]]
+                                        .value;
                                 }
                                 // eslint-disable-next-line no-console
                                 console.log(customName);
