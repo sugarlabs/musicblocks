0749e346041a43c370e8801eca2421f0c32844a2
Vanshika <vanshika@198625@gmail.com>
2026-01-26 23:48:23 +0530

    fix: Remove duplicate abcReader.onload event handler
    - Removed duplicate abcReader.onload assignment (lines 7219-7232)
    - Prevents potential memory leaks from duplicate event listeners
    - Eliminates unpredictable behavior in ABC music file parsing
    - Keeps single event handler definition for clean code
    
    Fixes potential race conditions when importing ABC notation files.

--- js/activity.js
+++ js/activity.js
@@ -2054,7 +2054,7 @@ class Activity {
             const changeText = () => {
                 const randomLoadMessage =
                     messages.load_messages[
-                        Math.floor(Math.random() * messages.load_messages.length)
+                    Math.floor(Math.random() * messages.load_messages.length)
                     ];
                 document.getElementById("messageText").innerHTML = randomLoadMessage + "...";
                 counter++;
@@ -2998,11 +2998,11 @@ class Activity {
                     .data("item.autocomplete", item)
                     .append(
                         '<img src="' +
-                            item.artwork +
-                            '" height="20px">' +
-                            "<a> " +
-                            item.label +
-                            "</a>"
+                        item.artwork +
+                        '" height="20px">' +
+                        "<a> " +
+                        item.label +
+                        "</a>"
                     )
                     .appendTo(
                         ul.css({
@@ -4355,8 +4355,8 @@ class Activity {
                         console.log(
                             "%cMusic Blocks",
                             "font-size: 24px; font-weight: bold; font-family: sans-serif; padding:20px 0 0 110px; background: url(" +
-                                imgUrl +
-                                ") no-repeat;"
+                            imgUrl +
+                            ") no-repeat;"
                         );
                         // eslint-disable-next-line no-console
                         console.log(
@@ -4428,10 +4428,10 @@ class Activity {
                 typeof flags !== "undefined"
                     ? flags
                     : {
-                          run: false,
-                          show: false,
-                          collapse: false
-                      };
+                        run: false,
+                        show: false,
+                        collapse: false
+                    };
             this.loading = true;
             document.body.style.cursor = "wait";
             this.doLoadAnimation();
@@ -4794,9 +4794,8 @@ class Activity {
                                 [
                                     "nameddo",
                                     {
-                                        value: `V: ${parseInt(lineId) + 1} Line ${
-                                            staffBlocksMap[lineId]?.baseBlocks?.length + 1
-                                        }`
+                                        value: `V: ${parseInt(lineId) + 1} Line ${staffBlocksMap[lineId]?.baseBlocks?.length + 1
+                                            }`
                                     }
                                 ],
                                 0,
@@ -4805,12 +4804,12 @@ class Activity {
                                     staffBlocksMap[lineId].baseBlocks.length === 0
                                         ? null
                                         : staffBlocksMap[lineId].baseBlocks[
-                                              staffBlocksMap[lineId].baseBlocks.length - 1
-                                          ][0][
-                                              staffBlocksMap[lineId].baseBlocks[
-                                                  staffBlocksMap[lineId].baseBlocks.length - 1
-                                              ][0].length - 4
-                                          ][0],
+                                        staffBlocksMap[lineId].baseBlocks.length - 1
+                                        ][0][
+                                        staffBlocksMap[lineId].baseBlocks[
+                                            staffBlocksMap[lineId].baseBlocks.length - 1
+                                        ][0].length - 4
+                                        ][0],
                                     null
                                 ]
                             ],
@@ -4826,9 +4825,8 @@ class Activity {
                                 [
                                     "text",
                                     {
-                                        value: `V: ${parseInt(lineId) + 1} Line ${
-                                            staffBlocksMap[lineId]?.baseBlocks?.length + 1
-                                        }`
+                                        value: `V: ${parseInt(lineId) + 1} Line ${staffBlocksMap[lineId]?.baseBlocks?.length + 1
+                                            }`
                                     }
                                 ],
                                 0,
@@ -4863,14 +4861,14 @@ class Activity {
                     staffBlocksMap[staffIndex].startBlock.length - 3
                 ][4][2] =
                     staffBlocksMap[staffIndex].baseBlocks[0][0][
-                        staffBlocksMap[staffIndex].baseBlocks[0][0].length - 4
+                    staffBlocksMap[staffIndex].baseBlocks[0][0].length - 4
                     ][0];
                 // Update the first namedo block with settimbre
                 staffBlocksMap[staffIndex].baseBlocks[0][0][
                     staffBlocksMap[staffIndex].baseBlocks[0][0].length - 4
                 ][4][0] =
                     staffBlocksMap[staffIndex].startBlock[
-                        staffBlocksMap[staffIndex].startBlock.length - 3
+                    staffBlocksMap[staffIndex].startBlock.length - 3
                     ][0];
                 const repeatblockids = staffBlocksMap[staffIndex].repeatArray;
                 for (const repeatId of repeatblockids) {
@@ -4882,7 +4880,7 @@ class Activity {
                             0,
                             [
                                 staffBlocksMap[staffIndex].startBlock[
-                                    staffBlocksMap[staffIndex].startBlock.length - 3
+                                staffBlocksMap[staffIndex].startBlock.length - 3
                                 ][0] /*setribmre*/,
                                 blockId + 1,
                                 staffBlocksMap[staffIndex].nameddoArray[staffIndex][0],
@@ -4891,8 +4889,8 @@ class Activity {
                                 ] === null
                                     ? null
                                     : staffBlocksMap[staffIndex].nameddoArray[staffIndex][
-                                          repeatId.end + 1
-                                      ]
+                                    repeatId.end + 1
+                                    ]
                             ]
                         ]);
                         staffBlocksMap[staffIndex].repeatBlock.push([
@@ -4926,7 +4924,7 @@ class Activity {
                             const secondnammedo = _searchIndexForMusicBlock(
                                 staffBlocksMap[staffIndex].baseBlocks[repeatId.end + 1][0],
                                 staffBlocksMap[staffIndex].nameddoArray[staffIndex][
-                                    repeatId.end + 1
+                                repeatId.end + 1
                                 ]
                             );
 
@@ -4950,13 +4948,13 @@ class Activity {
                         const prevnameddo = _searchIndexForMusicBlock(
                             staffBlocksMap[staffIndex].baseBlocks[repeatId.start - 1][0],
                             staffBlocksMap[staffIndex].baseBlocks[repeatId.start][0][
-                                currentnammeddo
+                            currentnammeddo
                             ][4][0]
                         );
                         const afternamedo = _searchIndexForMusicBlock(
                             staffBlocksMap[staffIndex].baseBlocks[repeatId.end][0],
                             staffBlocksMap[staffIndex].baseBlocks[repeatId.start][0][
-                                currentnammeddo
+                            currentnammeddo
                             ][4][1]
                         );
                         let prevrepeatnameddo = -1;
@@ -4964,17 +4962,17 @@ class Activity {
                             prevrepeatnameddo = _searchIndexForMusicBlock(
                                 staffBlocksMap[staffIndex].repeatBlock,
                                 staffBlocksMap[staffIndex].baseBlocks[repeatId.start][0][
-                                    currentnammeddo
+                                currentnammeddo
                                 ][4][0]
                             );
                         }
                         const prevBlockId =
                             staffBlocksMap[staffIndex].baseBlocks[repeatId.start][0][
-                                currentnammeddo
+                            currentnammeddo
                             ][4][0];
                         const currentBlockId =
                             staffBlocksMap[staffIndex].baseBlocks[repeatId.start][0][
-                                currentnammeddo
+                            currentnammeddo
                             ][0];
 
                         // Needs null checking optmizie
@@ -4988,7 +4986,7 @@ class Activity {
                             0,
                             [
                                 staffBlocksMap[staffIndex].baseBlocks[repeatId.start][0][
-                                    currentnammeddo
+                                currentnammeddo
                                 ][4][0],
                                 blockId + 1,
                                 currentBlockId,
@@ -5603,7 +5601,7 @@ class Activity {
             this.update = true;
         };
 
-        this.__showAltoAccidentals = () => {};
+        this.__showAltoAccidentals = () => { };
 
         /*
          * Shows musical alto staff
@@ -6270,12 +6268,12 @@ class Activity {
                     .data("item.autocomplete", item)
                     .append(
                         '<img src="' +
-                            item.artwork +
-                            '" height = "20px">' +
-                            "<a>" +
-                            " " +
-                            item.label +
-                            "</a>"
+                        item.artwork +
+                        '" height = "20px">' +
+                        "<a>" +
+                        " " +
+                        item.label +
+                        "</a>"
                     )
                     .appendTo(ul.css("z-index", 9999));
             };
@@ -6390,10 +6388,10 @@ class Activity {
             container.setAttribute(
                 "style",
                 "position: absolute; right:" +
-                    (document.body.clientWidth - x) +
-                    "px;  top: " +
-                    y +
-                    "px;"
+                (document.body.clientWidth - x) +
+                "px;  top: " +
+                y +
+                "px;"
             );
             document.getElementById("buttoncontainerBOTTOM").appendChild(container);
             return container;
@@ -7203,21 +7201,6 @@ class Activity {
                     // eslint-disable-next-line no-console
                     console.debug(midi);
                     midiImportBlocks(midi);
-                };
-
-                // Music Block Parser from abc to MB
-                abcReader.onload = event => {
-                    //get the abc data and replace the / so that the block does not break
-                    let abcData = event.target.result;
-                    abcData = abcData.replace(/\\/g, "");
-
-                    const tunebook = new ABCJS.parseOnly(abcData);
-                    // eslint-disable-next-line no-console
-                    console.log(tunebook);
-                    tunebook.forEach(tune => {
-                        //call parseABC to parse abcdata to MB json
-                        this.parseABC(tune);
-                    });
                 };
 
                 // Music Block Parser from abc to MB
