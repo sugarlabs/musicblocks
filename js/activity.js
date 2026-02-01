c619db61d6e1279c596185c44fb77975eb422d24
shreyash Yadav <shreyashy49@gmail.com>
2026-02-01 15:46:08 +0530

    Fixes a runtime error where  is not defined on the Planet page.

--- js/activity.js
+++ js/activity.js
@@ -7676,12 +7676,16 @@ class Activity {
 
 const activity = new Activity();
 
+// expose legacy global used by index.html
+window.doSearch = activity.doSearch;
+
 // Execute initialization once all RequireJS modules are loaded AND DOM is ready
 define(["domReady!"].concat(MYDEFINES), doc => {
     const initialize = () => {
         if (typeof createDefaultStack !== "undefined") {
             activity.setupDependencies();
             activity.domReady(doc);
+            activity.doSearch();
             activity.doContextMenus();
             activity.doPluginsAndPaletteCols();
         } else {
