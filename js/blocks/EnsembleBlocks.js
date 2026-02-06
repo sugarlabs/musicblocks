62058a85aa4e34fed1273f95dd9df5eb80b289bc
Parth <dagia@Parths-MacBook-Pro.local>
2026-02-06 15:58:18 +0530

    error in master branch, changes in 2025 to 2026 in license

--- js/blocks/EnsembleBlocks.js
+++ js/blocks/EnsembleBlocks.js
@@ -905,9 +905,8 @@ function setupEnsembleBlocks(activity) {
                     activity.errorMsg(_("Cannot find turtle") + " " + args[0], blk);
                 }
             } else {
-                activity.turtles.ithTurtle(turtle).singer.turtleTime = activity.turtles.ithTurtle(
-                    targetTurtle
-                ).singer.turtleTime;
+                activity.turtles.ithTurtle(turtle).singer.turtleTime =
+                    activity.turtles.ithTurtle(targetTurtle).singer.turtleTime;
             }
         }
     }
