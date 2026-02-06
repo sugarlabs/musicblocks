4ea73d26ee233061f5fdd3fdfc93e3291159f475
Vanshika <pahalvanshikaa@gmail.com>
2026-02-06 23:43:26 +0530

    chore: run Prettier to fix formatting issues in activity.js

--- js/activity.js
+++ js/activity.js
@@ -2046,7 +2046,7 @@ class Activity {
             const changeText = () => {
                 const randomLoadMessage =
                     messages.load_messages[
-                    Math.floor(Math.random() * messages.load_messages.length)
+                        Math.floor(Math.random() * messages.load_messages.length)
                     ];
                 document.getElementById("messageText").innerHTML = randomLoadMessage + "...";
                 counter++;
@@ -2997,11 +2997,11 @@ class Activity {
                         return $j("<li></li>")
                             .append(
                                 '<img src="' +
-                                (item.artwork || "") +
-                                '" height="20px">' +
-                                "<a> " +
-                                item.label +
-                                "</a>"
+                                    (item.artwork || "") +
+                                    '" height="20px">' +
+                                    "<a> " +
+                                    item.label +
+                                    "</a>"
                             )
                             .appendTo(
                                 ul.css({
@@ -3014,7 +3014,6 @@ class Activity {
                 }
                 $search.data("autocomplete-init", true);
             }
-
 
             const searchInput = this.searchWidget.idInput_custom;
             if (!searchInput || searchInput.length <= 0) {
@@ -4373,8 +4372,8 @@ class Activity {
                         console.log(
                             "%cMusic Blocks",
                             "font-size: 24px; font-weight: bold; font-family: sans-serif; padding:20px 0 0 110px; background: url(" +
-                            imgUrl +
-                            ") no-repeat;"
+                                imgUrl +
+                                ") no-repeat;"
                         );
                         // eslint-disable-next-line no-console
                         console.log(
@@ -4446,10 +4445,10 @@ class Activity {
                 typeof flags !== "undefined"
                     ? flags
                     : {
-                        run: false,
-                        show: false,
-                        collapse: false
-                    };
+                          run: false,
+                          show: false,
+                          collapse: false
+                      };
             this.loading = true;
             document.body.style.cursor = "wait";
             this.doLoadAnimation();
@@ -4812,8 +4811,9 @@ class Activity {
                                 [
                                     "nameddo",
                                     {
-                                        value: `V: ${parseInt(lineId) + 1} Line ${staffBlocksMap[lineId]?.baseBlocks?.length + 1
-                                            }`
+                                        value: `V: ${parseInt(lineId) + 1} Line ${
+                                            staffBlocksMap[lineId]?.baseBlocks?.length + 1
+                                        }`
                                     }
                                 ],
                                 0,
@@ -4822,12 +4822,12 @@ class Activity {
                                     staffBlocksMap[lineId].baseBlocks.length === 0
                                         ? null
                                         : staffBlocksMap[lineId].baseBlocks[
-                                        staffBlocksMap[lineId].baseBlocks.length - 1
-                                        ][0][
-                                        staffBlocksMap[lineId].baseBlocks[
-                                            staffBlocksMap[lineId].baseBlocks.length - 1
-                                        ][0].length - 4
-                                        ][0],
+                                              staffBlocksMap[lineId].baseBlocks.length - 1
+                                          ][0][
+                                              staffBlocksMap[lineId].baseBlocks[
+                                                  staffBlocksMap[lineId].baseBlocks.length - 1
+                                              ][0].length - 4
+                                          ][0],
                                     null
                                 ]
                             ],
@@ -4843,8 +4843,9 @@ class Activity {
                                 [
                                     "text",
                                     {
-                                        value: `V: ${parseInt(lineId) + 1} Line ${staffBlocksMap[lineId]?.baseBlocks?.length + 1
-                                            }`
+                                        value: `V: ${parseInt(lineId) + 1} Line ${
+                                            staffBlocksMap[lineId]?.baseBlocks?.length + 1
+                                        }`
                                     }
                                 ],
                                 0,
@@ -4879,14 +4880,14 @@ class Activity {
                     staffBlocksMap[staffIndex].startBlock.length - 3
                 ][4][2] =
                     staffBlocksMap[staffIndex].baseBlocks[0][0][
-                    staffBlocksMap[staffIndex].baseBlocks[0][0].length - 4
+                        staffBlocksMap[staffIndex].baseBlocks[0][0].length - 4
                     ][0];
                 // Update the first namedo block with settimbre
                 staffBlocksMap[staffIndex].baseBlocks[0][0][
                     staffBlocksMap[staffIndex].baseBlocks[0][0].length - 4
                 ][4][0] =
                     staffBlocksMap[staffIndex].startBlock[
-                    staffBlocksMap[staffIndex].startBlock.length - 3
+                        staffBlocksMap[staffIndex].startBlock.length - 3
                     ][0];
                 const repeatblockids = staffBlocksMap[staffIndex].repeatArray;
                 for (const repeatId of repeatblockids) {
@@ -4898,7 +4899,7 @@ class Activity {
                             0,
                             [
                                 staffBlocksMap[staffIndex].startBlock[
-                                staffBlocksMap[staffIndex].startBlock.length - 3
+                                    staffBlocksMap[staffIndex].startBlock.length - 3
                                 ][0] /*setribmre*/,
                                 blockId + 1,
                                 staffBlocksMap[staffIndex].nameddoArray[staffIndex][0],
@@ -4907,8 +4908,8 @@ class Activity {
                                 ] === null
                                     ? null
                                     : staffBlocksMap[staffIndex].nameddoArray[staffIndex][
-                                    repeatId.end + 1
-                                    ]
+                                          repeatId.end + 1
+                                      ]
                             ]
                         ]);
                         staffBlocksMap[staffIndex].repeatBlock.push([
@@ -4942,7 +4943,7 @@ class Activity {
                             const secondnammedo = _searchIndexForMusicBlock(
                                 staffBlocksMap[staffIndex].baseBlocks[repeatId.end + 1][0],
                                 staffBlocksMap[staffIndex].nameddoArray[staffIndex][
-                                repeatId.end + 1
+                                    repeatId.end + 1
                                 ]
                             );
 
@@ -4965,13 +4966,13 @@ class Activity {
                         const prevnameddo = _searchIndexForMusicBlock(
                             staffBlocksMap[staffIndex].baseBlocks[repeatId.start - 1][0],
                             staffBlocksMap[staffIndex].baseBlocks[repeatId.start][0][
-                            currentnammeddo
+                                currentnammeddo
                             ][4][0]
                         );
                         const afternamedo = _searchIndexForMusicBlock(
                             staffBlocksMap[staffIndex].baseBlocks[repeatId.end][0],
                             staffBlocksMap[staffIndex].baseBlocks[repeatId.start][0][
-                            currentnammeddo
+                                currentnammeddo
                             ][4][1]
                         );
                         let prevrepeatnameddo = -1;
@@ -4979,17 +4980,17 @@ class Activity {
                             prevrepeatnameddo = _searchIndexForMusicBlock(
                                 staffBlocksMap[staffIndex].repeatBlock,
                                 staffBlocksMap[staffIndex].baseBlocks[repeatId.start][0][
-                                currentnammeddo
+                                    currentnammeddo
                                 ][4][0]
                             );
                         }
                         const prevBlockId =
                             staffBlocksMap[staffIndex].baseBlocks[repeatId.start][0][
-                            currentnammeddo
+                                currentnammeddo
                             ][4][0];
                         const currentBlockId =
                             staffBlocksMap[staffIndex].baseBlocks[repeatId.start][0][
-                            currentnammeddo
+                                currentnammeddo
                             ][0];
 
                         // Needs null checking optmizie
@@ -5003,7 +5004,7 @@ class Activity {
                             0,
                             [
                                 staffBlocksMap[staffIndex].baseBlocks[repeatId.start][0][
-                                currentnammeddo
+                                    currentnammeddo
                                 ][4][0],
                                 blockId + 1,
                                 currentBlockId,
@@ -6294,19 +6295,18 @@ class Activity {
                         return $j("<li></li>")
                             .append(
                                 '<img src="' +
-                                (item.artwork || "") +
-                                '" height = "20px">' +
-                                "<a>" +
-                                " " +
-                                item.label +
-                                "</a>"
+                                    (item.artwork || "") +
+                                    '" height = "20px">' +
+                                    "<a>" +
+                                    " " +
+                                    item.label +
+                                    "</a>"
                             )
                             .appendTo(ul.css("z-index", 35000));
                     };
                 }
                 $helpfulSearch.data("autocomplete-init", true);
             }
-
 
             const searchInput = this.helpfulSearchWidget.idInput_custom;
             if (!searchInput || searchInput.length <= 0) {
@@ -6424,10 +6424,10 @@ class Activity {
             container.setAttribute(
                 "style",
                 "position: absolute; right:" +
-                (document.body.clientWidth - x) +
-                "px;  top: " +
-                y +
-                "px;"
+                    (document.body.clientWidth - x) +
+                    "px;  top: " +
+                    y +
+                    "px;"
             );
             document.getElementById("buttoncontainerBOTTOM").appendChild(container);
             return container;
