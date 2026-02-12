e0094a625ba7fc82d5e4226cce4a00b3fac949b1
Parth Dagia <parth.24bcs10414@sst.scaler.com>
2026-02-13 01:55:18 +0530

    Fix formatting in musicutils.test.js

--- js/utils/__tests__/musicutils.test.js
+++ js/utils/__tests__/musicutils.test.js
@@ -2311,7 +2311,7 @@ describe("MUSICALMODES", () => {
     it("should have aeolian equivalent to minor", () => {
         expect(MUSICALMODES["aeolian"]).toEqual(MUSICALMODES["minor"]);
     });
-    
+});
 describe("getStepSizeDown", () => {
     it("should return the correct step size for D in C major going down", () => {
         const result = getStepSizeDown("C major", "D", 0, "equal");
