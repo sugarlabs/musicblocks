8f17464605f727c40e27bcde0ab0cd6e0ecaf31b
Vanshika <pahalvanshikaa@gmail.com>
2026-02-14 06:44:42 +0530

    chore: fix formatting in js/palette.js

--- js/palette.js
+++ js/palette.js
@@ -865,8 +865,9 @@ class Palette {
     }
 
     hideMenu() {
-        docById("palette").childNodes[0].style.borderRight =
-            `1px solid ${platformColor.selectorSelected}`;
+        docById(
+            "palette"
+        ).childNodes[0].style.borderRight = `1px solid ${platformColor.selectorSelected}`;
         if (this._outsideClickListener) {
             document.removeEventListener("click", this._outsideClickListener);
             this._outsideClickListener = null;
