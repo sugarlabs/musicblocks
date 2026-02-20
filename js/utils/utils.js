98190944e03f392b55942106c0999ceab7425f46
Kartik <kartiktripathi103@gmail.com>
2026-02-21 03:35:00 +0530

    fix: formatting with prettier

--- js/utils/utils.js
+++ js/utils/utils.js
@@ -1614,10 +1614,10 @@ let hexToRGB = hex => {
     const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
     return result
         ? {
-            r: parseInt(result[1], 16),
-            g: parseInt(result[2], 16),
-            b: parseInt(result[3], 16)
-        }
+              r: parseInt(result[1], 16),
+              g: parseInt(result[2], 16),
+              b: parseInt(result[3], 16)
+          }
         : null;
 };
 
