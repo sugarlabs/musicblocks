c18d3bb9b30055b2dbce6248e9379ffd4f973c88
Rohit-rk07 <rohitchaudharyrk.25@gmail.com>
2026-02-21 21:28:00 +0530

    fix(musickeyboard): cleanup metronome on widget close
    Call stopMetronome() in widget onclose to ensure countdown interval and loop resources are always released.
    
    Removes duplicate loop stop from onclose since cleanup is centralized in stopMetronome().

--- js/widgets/musickeyboard.js
+++ js/widgets/musickeyboard.js
@@ -647,12 +647,16 @@ function MusicKeyboard(activity) {
                 myNode.innerHTML = "";
             }
 
-            this.tick = false;
-            this.firstNote = false;
-            this.metronomeON = false;
+            // Ensure countdown interval/loop resources are cleaned up on close.
+            if (typeof this.stopMetronome === "function") {
+                this.stopMetronome();
+            } else {
+                this.tick = false;
+                this.firstNote = false;
+                this.metronomeON = false;
+            }
 
             selectedNotes = [];
-            if (this.loopTick) this.loopTick.stop();
             docById("wheelDivptm").style.display = "none";
             docById("wheelDivptm").style.display = "none";
             if (this._menuWheel) this._menuWheel.removeWheel();
