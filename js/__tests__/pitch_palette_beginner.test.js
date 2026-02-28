82f1b0f452226fe08a4b6aa6f2e220df209b0936
Yash Barhate <yshop857@gmail.com>
2026-02-28 23:30:07 +0530

    Add Pitch Palette Beginner Tests

---
+++ js/__tests__/pitch_palette_beginner.test.js
@@ -0,0 +1,18 @@
+describe("Pitch Palette Beginner", () => {
+
+  test("Test pitch sol", () => {
+    const pitch = "sol";
+    expect(pitch).toBe("sol");
+  });
+
+  test("Test pitch la", () => {
+    const pitch = "la";
+    expect(pitch).toBe("la");
+  });
+
+  test("Test pitch do", () => {
+    const pitch = "do";
+    expect(pitch).toBe("do");
+  });
+
+});
