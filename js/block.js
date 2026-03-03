af636938ff806001afe49472ea5bd86717762d53
Shreya Saxena <shreyasaxena2605@gmail.com>
2026-03-03 14:26:39 +0530

    Fix undefined pause() call in updateBounds

--- js/block.js
+++ js/block.js
@@ -415,7 +415,7 @@ class Block {
 
                     if (that.bounds === null) {
                         const delayTime = INITIAL_DELAY * Math.pow(2, loopCount);
-                        await that.pause(delayTime);
+                        await new Promise(resolve => setTimeout(resolve, delayTime));
                         updateBounds(loopCount + 1);
                     } else {
                         that.container.updateCache();
