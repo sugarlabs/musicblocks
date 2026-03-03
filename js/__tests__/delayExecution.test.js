94a48abd39445112dec114948455e42912765b31
Mishti Agarwal <mishtiagarwal02@gmail.com>
2026-03-04 00:59:31 +0530

    Add unit tests for delayExecution utility function

---
+++ js/__tests__/delayExecution.test.js
@@ -0,0 +1,43 @@
+const { delayExecution } = require("../utils/utils");
+
+describe("delayExecution utility", () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+
+  afterEach(() => {
+    jest.useRealTimers();
+  });
+
+  test("resolves after specified delay", async () => {
+    const promise = delayExecution(1000);
+
+    jest.advanceTimersByTime(1000);
+
+    await expect(promise).resolves.toBe(true);
+  });
+
+  test("does not resolve before delay", async () => {
+    const promise = delayExecution(1000);
+
+    jest.advanceTimersByTime(500);
+
+    let resolved = false;
+    promise.then(() => (resolved = true));
+
+    await Promise.resolve();
+
+    expect(resolved).toBe(false);
+  });
+
+  test("multiple calls resolve independently", async () => {
+    const p1 = delayExecution(500);
+    const p2 = delayExecution(1000);
+
+    jest.advanceTimersByTime(500);
+    await expect(p1).resolves.toBe(true);
+
+    jest.advanceTimersByTime(500);
+    await expect(p2).resolves.toBe(true);
+  });
+});
