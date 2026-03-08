99bd8f2acfb26bc9f961ca078587dce64872e1fd
Rohit-rk07 <rohitchaudharyrk.25@gmail.com>
2026-03-08 19:15:23 +0530

    style(utils): run prettier on camera capture cleanup

--- js/utils/utils.js
+++ js/utils/utils.js
@@ -1174,22 +1174,22 @@ let doUseCamera = (args, turtles, turtle, isVideo, cameraID, setCameraID, errorM
     }
 
     CameraManager.setCanplayListener(video, () => {
-            // console.debug("canplay", streaming, CameraManager.isSetup);
-            if (!streaming) {
-                video.setAttribute("width", w);
-                video.setAttribute("height", h);
-                canvas.setAttribute("width", w);
-                canvas.setAttribute("height", h);
-                streaming = true;
+        // console.debug("canplay", streaming, CameraManager.isSetup);
+        if (!streaming) {
+            video.setAttribute("width", w);
+            video.setAttribute("height", h);
+            canvas.setAttribute("width", w);
+            canvas.setAttribute("height", h);
+            streaming = true;
 
-                if (isVideo) {
-                    CameraManager.startCapture(draw, setCameraID);
-                } else {
-                    CameraManager.stopCapture(setCameraID);
-                    draw();
-                }
+            if (isVideo) {
+                CameraManager.startCapture(draw, setCameraID);
+            } else {
+                CameraManager.stopCapture(setCameraID);
+                draw();
             }
-        });
+        }
+    });
 };
 
 /**
