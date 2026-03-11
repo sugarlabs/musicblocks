3e24147d1e9fdcecfd5495c77e396f44cdc0bbf7
Abhnish Kumar <abhnish.1289@gmail.com>
2026-03-12 00:28:02 +0530

    style: reformat toolbar.test.js with prettier v3

--- js/__tests__/toolbar.test.js
+++ js/__tests__/toolbar.test.js
@@ -291,8 +291,8 @@ describe("Toolbar Class", () => {
             id === "stop"
                 ? stopIcon
                 : id === "record"
-                ? recordButton
-                : { addEventListener: jest.fn(), querySelectorAll: jest.fn() }
+                  ? recordButton
+                  : { addEventListener: jest.fn(), querySelectorAll: jest.fn() }
         );
 
         const mockOnClick = jest.fn();
