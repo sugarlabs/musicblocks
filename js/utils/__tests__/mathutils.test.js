62c6493644cffa2d1d0bfadc7671819bddaee690
Neethi Hena <neethihenavalaparla03@gmail.com>
2026-03-16 02:42:12 +0530

    Fix mathutils test

--- js/utils/__tests__/mathutils.test.js
+++ js/utils/__tests__/mathutils.test.js
@@ -15,6 +15,7 @@
  *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ * /* global MULTIPALETTES, platformColor, docById, TEXTWIDTH
  */
 
 // A basic test file in jest framework for the mathutils.js
