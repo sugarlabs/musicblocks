86f0779d29fdf176c952c345ef102bc5962b6e67
Abhnish Kumar <abhnish.1289@gmail.com>
2026-03-18 09:42:11 +0530

    Fix ESLint: add global declarations for NOACTIONERRORMSG and NOINPUTERRORMSG

--- js/blocks/__tests__/ActionBlocks.test.js
+++ js/blocks/__tests__/ActionBlocks.test.js
@@ -20,6 +20,7 @@
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
+/* global NOACTIONERRORMSG, NOINPUTERRORMSG */
 const { setupActionBlocks } = jest.requireActual("../ActionBlocks");
 
 global._ = s => s;
