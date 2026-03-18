2eee36bef3490dc07cfdc24569f2d489d00326ec
Ady0333 <adityashinde1525@gmail.com>
2026-03-19 00:51:53 +0530

    PoC: Implement root-relative frequency computation for Just Intonation chords
    Add TemperamentEngine module for dynamic frequency calculation.
    Fixes syntonic comma error in non-root JI chords.
    60 tests pass, lint clean.

--- js/utils/__tests__/temperament-engine.test.js
+++ js/utils/__tests__/temperament-engine.test.js
@@ -1,13 +1,5 @@
-// Copyright (c) 2026 Aditya
-//
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the The GNU Affero General Public
-// License as published by the Free Software Foundation; either
-// version 3 of the License, or (at your option) any later version.
-//
-// You should have received a copy of the GNU Affero General Public
-// License along with this library; if not, write to the Free Software
-// Foundation, 51 Franklin Street, Suite 500 Boston, MA 02110-1335 USA
+// PoC: Root-relative frequency computation for Just Intonation chords
+// GSoC 2026 - Music Blocks Temperament Backport
 
 /*
    TemperamentEngine PoC tests.
