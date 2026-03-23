04938c85c3cc4c976c3bbebb80d4e2c686e6b6a0
mahesh-09-12 <m8143177@gmail.com>
2026-03-23 16:07:13 +0530

    test: configure Jest for shared setup - enable setupFilesAfterEnv - use clearMocks and restoreMocks

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,14 @@
 module.exports = {
     testMatch: ["**/__tests__/**/*.test.js", "**/?(*.)+(spec|test).[jt]s?(x)"],
     clearMocks: true,
+    restoreMocks: true,
+
     moduleFileExtensions: ["js", "json", "node"],
     testEnvironment: "jsdom",
+
+    setupFilesAfterEnv: ["<rootDir>/test/setupTests.js"],
+
     collectCoverage: true,
-    collectCoverageFrom: [
-        "js/**/*.js",
-        "!js/vendor/**",
-    ],
-    coverageReporters: ["text-summary", "text", "lcov"],
+    collectCoverageFrom: ["js/**/*.js", "!js/vendor/**"],
+    coverageReporters: ["text-summary", "text", "lcov"]
 };
