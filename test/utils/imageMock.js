0e2a40c8acb34fd25c107e4d0b7bdcbe583e0199
mahesh-09-12 <m8143177@gmail.com>
2026-03-23 16:08:30 +0530

    test(palette): refactor tests to use shared utilities
    - replace inline mocks
    - improve DOM handling

--- test/utils/imageMock.js
+++ test/utils/imageMock.js
@@ -1,7 +1,21 @@
 function setupImageMock() {
     global.Image = function () {
         const img = document.createElement("img");
+
         img.width = 50;
+
+        if (!img.setAttribute) {
+            img.setAttribute = jest.fn();
+        }
+
+        if (!img.removeAttribute) {
+            img.removeAttribute = jest.fn();
+        }
+
+        if (!img.style) {
+            img.style = {};
+        }
+
         return img;
     };
 }
