92284d4a37014b9d689aa1919f2201982a4b3866
swapnachoudhary43 <sapnachoudhary8243@gmail.com>
2026-03-30 01:20:22 +0530

    Fix: use window/global.performanceTrackerInstance instead of AMD require and bare performanceTracker references

--- js/logo.js
+++ js/logo.js
@@ -32,9 +32,16 @@
 /*
    exported
 
-   Queue, Logo, LogoDependencies
- */
-
+   Queue, Logo, LogoDependencies, DEFAULTVOLUME, PREVIEWVOLUME, DEFAULTDELAY,
+   OSCVOLUMEADJUSTMENT, TONEBPM, TARGETBPM, TURTLESTEP, NOTEDIV,
+   MIN_HIGHLIGHT_DURATION_MS,
+   NOMICERRORMSG, NANERRORMSG, NOSTRINGERRORMSG, NOBOXERRORMSG,
+   NOACTIONERRORMSG, NOINPUTERRORMSG, NOSQRTERRORMSG,
+   ZERODIVIDEERRORMSG, EMPTYHEAPERRORMSG, INVALIDPITCH, POSNUMBER,run
+   NOTATIONNOTE, NOTATIONDURATION, NOTATIONDOTCOUNT,
+   NOTATIONTUPLETVALUE, NOTATIONROUNDDOWN, NOTATIONINSIDECHORD,
+   NOTATIONSTACCATO
+*/
 // Constants moved to js/logoconstants.js to resolve circular dependency
 
 /**
@@ -1228,7 +1235,6 @@ class Logo {
                 performanceTracker.enable();
             }
         }
-
         this._prematureRestart = this._alreadyRunning;
 
         if (this._alreadyRunning && this._runningBlock !== null) {
@@ -1427,8 +1433,15 @@ class Logo {
         }
 
         // Performance instrumentation: begin tracking
-        if (window.performanceTrackerInstance) {
-            window.performanceTrackerInstance.startRun();
+        const tracker =
+            typeof window !== "undefined"
+                ? window.performanceTrackerInstance
+                : typeof global !== "undefined"
+                  ? global.performanceTrackerInstance
+                  : null;
+
+        if (tracker) {
+            tracker.enterBlock();
         }
         /*
         ===========================================================================
@@ -1767,9 +1780,13 @@ class Logo {
                 if (cf !== undefined) childFlow = cf;
                 if (cfc !== undefined) childFlowCount = cfc;
                 if (ret) {
-                    if (typeof performanceTracker !== "undefined") {
-                        performanceTracker.exitBlock();
-                    }
+                    const _pt3 =
+                        typeof window !== "undefined"
+                            ? window.performanceTrackerInstance
+                            : typeof global !== "undefined"
+                              ? global.performanceTrackerInstance
+                              : null;
+                    if (_pt3) _pt3.exitBlock();
                     return ret;
                 }
             }
@@ -2045,9 +2062,15 @@ class Logo {
                     tur.singer.justCounting.length === 0
                 ) {
                     // Performance instrumentation: end tracking and log stats
-                    if (typeof performanceTracker !== "undefined") {
-                        performanceTracker.endRun();
-                        performanceTracker.logStats();
+                    const _pt4 =
+                        typeof window !== "undefined"
+                            ? window.performanceTrackerInstance
+                            : typeof global !== "undefined"
+                              ? global.performanceTrackerInstance
+                              : null;
+                    if (_pt4) {
+                        _pt4.endRun();
+                        _pt4.logStats();
                     }
 
                     if (logo.runningLilypond) {
@@ -2140,9 +2163,13 @@ class Logo {
             logo._timerManager.setTimeout(__checkCompletionState, 100);
         }
 
-        if (typeof performanceTracker !== "undefined") {
-            performanceTracker.exitBlock();
-        }
+        const _pt5 =
+            typeof window !== "undefined"
+                ? window.performanceTrackerInstance
+                : typeof global !== "undefined"
+                  ? global.performanceTrackerInstance
+                  : null;
+        if (_pt5) _pt5.exitBlock();
     }
 
     /**
