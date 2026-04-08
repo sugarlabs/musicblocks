002ac71deeefa7e3473485df4359f4650d708354
SidharthMishra <mishrasid2022@gmail.com>
2026-04-08 18:45:31 +0530

    Format lighthouserc.js using Prettier

--- lighthouserc.js
+++ lighthouserc.js
@@ -73,7 +73,6 @@ module.exports = {
             assertions: {
                 ...sharedAssertions,
                 ...performanceAssertions
-               
             }
         },
         upload: {
