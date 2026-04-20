d2a36d50167750c7c1ebbbf986ca3ba8469a1c5a
Karthik-dev56 <prokarthik1889@gmail.com>
2026-04-21 03:04:50 +0530

    fix: resolve ReferenceError and accidental handling logic in synthutils tests

--- js/utils/synthutils.js
+++ js/utils/synthutils.js
@@ -1356,7 +1356,8 @@ function Synth() {
         const solfegeDict = { do: 0, re: 2, mi: 4, fa: 5, sol: 7, la: 9, ti: 11 };
         const letterDict = { C: 0, D: 2, E: 4, F: 5, G: 7, A: 9, B: 11 };
 
-        let attr = getArticulation(solfege);
+        let articulationChar = getArticulation(solfege);
+        let attr = articulationChar;
         if (attr === SHARP) {
             attr = 1;
         } else if (attr === FLAT) {
@@ -1369,7 +1370,7 @@ function Synth() {
             attr = 0;
         }
 
-        const fragment = solfege.replace(attr, "");
+        const fragment = solfege.replace(articulationChar, "");
         let chromaticNumber = 0;
         if (fragment in solfegeDict) {
             chromaticNumber = solfegeDict[fragment];
