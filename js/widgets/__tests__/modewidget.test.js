117bb47a434a86af12a3c276548835ca98d9a7a3
e-esakman <sumansakshee36@gmail.com>
2026-04-27 14:23:47 +0530

    add address in license

--- js/widgets/__tests__/modewidget.test.js
+++ js/widgets/__tests__/modewidget.test.js
@@ -18,6 +18,10 @@
  *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, 51 Franklin Street, Suite 500 Boston, MA 02110-1335 USA
  */
 
 const ModeWidget = require("../modewidget.js");
