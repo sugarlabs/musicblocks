161e55820762c392bd6e8ad6e53865fe932da3f7
Dhruv_Prajapati <dhruvprajapati3016@gmail.com>
2026-04-28 14:10:00 +0530

    Fix formatting and add missing newline in planetInterface.js

--- js/planetInterface.js
+++ js/planetInterface.js
@@ -4,6 +4,7 @@
 // modify it under the terms of the The GNU Affero General Public
 // License as published by the Free Software Foundation; either
 // version 3 of the License, or (at your option) any later version.
+
 //
 // You should have received a copy of the GNU Affero General Public
 // License along with this library; if not, write to the Free Software
@@ -347,3 +348,4 @@ class PlanetInterface {
 if (typeof module !== "undefined" && module.exports) {
     module.exports = PlanetInterface;
 }
+
