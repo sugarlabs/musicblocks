2003afd97b92225e5d1ba7b353465b4da70b6ff1
Abhnish Kumar <abhnish.1289@gmail.com>
2026-06-28 06:50:31 +0530

    feat: announce program run/stop state via aria-live region (#6608)
    Signed-off-by: Abhnish Kumar <abhnish.1289@gmail.com>

--- js/activity.js
+++ js/activity.js
@@ -2094,7 +2094,8 @@ class Activity {
             if (recordBtn) {
                 recordBtn.classList.remove("grey-text", "inactiveLink");
             }
-
+            // Announce program stop to screen readers
+            this.textMsg && this.textMsg(_("Program stopped."));
             // TODO: plugin support
         };
 
@@ -2103,6 +2104,8 @@ class Activity {
          */
         this.onRunTurtle = () => {
             // TODO: plugin support
+            // Announce program start to screen readers
+            this.textMsg && this.textMsg(_("Program running."));
         };
 
         /*
