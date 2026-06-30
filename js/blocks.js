d890ac54e57d679b8cfd144ea50f60ea57571538
rvkeerthi24 <rvkeerthi99@gmail.com>
2026-06-30 19:51:59 +0530

    fix: resolve musickeyboard clicks desync and connection crashes

--- js/blocks.js
+++ js/blocks.js
@@ -1048,8 +1048,8 @@ class Blocks {
              * These checks are to test for malformed data. All blocks
              * should have connections.
              */
-            if (myBlock === null) {
-                console.debug("Saw a null block: " + blk);
+            if (myBlock === null || myBlock === undefined) {
+                console.debug("Saw a null or undefined block: " + blk);
                 if (isOuterCall) this._endDeferCheckBounds();
                 return;
             }
@@ -1105,8 +1105,10 @@ class Blocks {
                 }
 
                 /** Another database integrity check. */
-                if (this.blockList[cblk] === null) {
-                    console.debug("This is not good: we encountered a null block: " + cblk);
+                if (this.blockList[cblk] === null || this.blockList[cblk] === undefined) {
+                    console.debug(
+                        "This is not good: we encountered a null or undefined block: " + cblk
+                    );
                     continue;
                 }
 
