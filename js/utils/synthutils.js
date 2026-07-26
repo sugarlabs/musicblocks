042d47170a92576eec31067db4b8ee86332cca30
shivv23 <shivamkumar0423@gmail.com>
2026-07-27 00:28:16 +0530

    fix: dispose mic/recorder in recording to prevent Tone.js leaks (Fixes #7917)

--- js/utils/synthutils.js
+++ js/utils/synthutils.js
@@ -2649,6 +2649,16 @@ function Synth() {
      */
     this.startRecording = async () => {
         await Tone.start();
+
+        if (this.mic) {
+            this.mic.close();
+            this.mic = null;
+        }
+        if (this.recorder) {
+            this.recorder.dispose();
+            this.recorder = null;
+        }
+
         this.mic = new Tone.UserMedia();
         this.recorder = new Tone.Recorder();
         await this.mic
@@ -2710,6 +2720,10 @@ function Synth() {
         _revokeRecordingURL();
         this.recording = await this.recorder.stop();
         this.mic.close();
+        this.mic.dispose();
+        this.mic = null;
+        this.recorder.dispose();
+        this.recorder = null;
         this.audioURL = URL.createObjectURL(this.recording);
         return this.audioURL;
     };
