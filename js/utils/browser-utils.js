220d8def985696fcf255456e111f5057cbf917b5
KARAN <karankumarshk7@gmail.com>
2026-08-01 04:31:06 +0530

    fix(browser-utils): correct Edge and Opera browser detection in fnBrowserDetect (#7878)

--- js/utils/browser-utils.js
+++ js/utils/browser-utils.js
@@ -34,16 +34,16 @@ function fnBrowserDetect() {
     const userAgent = navigator.userAgent;
     let browserName;
 
-    if (userAgent.match(/chrome|chromium|crios/i)) {
+    if (userAgent.match(/edg/i)) {
+        browserName = "edge";
+    } else if (userAgent.match(/opr\//i)) {
+        browserName = "opera";
+    } else if (userAgent.match(/chrome|chromium|crios/i)) {
         browserName = "chrome";
     } else if (userAgent.match(/firefox|fxios/i)) {
         browserName = "firefox";
     } else if (userAgent.match(/safari/i)) {
         browserName = "safari";
-    } else if (userAgent.match(/opr\//i)) {
-        browserName = "opera";
-    } else if (userAgent.match(/edg/i)) {
-        browserName = "edge";
     } else {
         browserName = "No browser detection";
     }
