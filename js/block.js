eda531ff60e6697310f539a4226abcc05d45b8a2
jetshree_sharma <jetshreesharma@gmail.com>
2026-08-24 02:06:13 +0530

    fix: break circular references on block stack deletion (#8171)
    Signed-off-by: Jetshree <jetshreesharma@gmail.com>

--- js/block.js
+++ js/block.js
@@ -548,6 +548,79 @@ class Block {
     }
 
     /**
+     * Clean up and dispose of block resources to prevent memory leaks and circular references
+     * @public
+     * @returns {void}
+     */
+    dispose() {
+        if (
+            Array.isArray(this.connections) &&
+            this.blocks &&
+            Array.isArray(this.blocks.blockList)
+        ) {
+            const thisIdx = this.blockIndex;
+            for (let i = 0; i < this.connections.length; i++) {
+                const connId = this.connections[i];
+                if (connId !== null && connId !== undefined && this.blocks.blockList[connId]) {
+                    const connBlock = this.blocks.blockList[connId];
+                    if (Array.isArray(connBlock.connections)) {
+                        for (let j = 0; j < connBlock.connections.length; j++) {
+                            if (connBlock.connections[j] === thisIdx) {
+                                connBlock.connections[j] = null;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        this.connections = [];
+
+        if (this.label && this.label.parentNode) {
+            this.label.parentNode.removeChild(this.label);
+        }
+        if (this.labelattr && this.labelattr.parentNode) {
+            this.labelattr.parentNode.removeChild(this.labelattr);
+        }
+        this.label = null;
+        this.labelattr = null;
+
+        if (this.container) {
+            if (typeof this.container.removeAllEventListeners === "function") {
+                this.container.removeAllEventListeners();
+            }
+            if (typeof this.container.removeAllChildren === "function") {
+                this.container.removeAllChildren();
+            }
+            if (typeof this.container.uncache === "function") {
+                this.container.uncache();
+            }
+            this.container = null;
+        }
+
+        this.bitmap = null;
+        this.highlightBitmap = null;
+        this.disconnectedBitmap = null;
+        this.disconnectedHighlightBitmap = null;
+        this.collapseButtonBitmap = null;
+        this.expandButtonBitmap = null;
+        this.collapseBlockBitmap = null;
+        this.highlightCollapseBlockBitmap = null;
+        this.imageBitmap = null;
+
+        this.artwork = null;
+        this.collapseArtwork = null;
+        this.text = null;
+        this.value = null;
+        this.privateData = null;
+        this.postProcessArg = null;
+        this.controller = null;
+
+        this.protoblock = null;
+        this.blocks = null;
+        this.activity = null;
+    }
+
+    /**
      * Show the highlight artwork.
      * @public
      * @returns {void}
