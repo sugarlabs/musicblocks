98485a2a21426614c55f1db33bd60ab2feabe7f1
severe77 <maityanubhab2007@gmail.com>
2026-09-02 15:10:16 +0000

    test: restore palette test header
    Signed-off-by: severe77 <maityanubhab2007@gmail.com>

--- js/__tests__/palette.test.js
+++ js/__tests__/palette.test.js
@@ -4,7 +4,7 @@
  * MusicBlocks v3.7.0
  * Copyright (C) 2025 Om Santosh Suneri
  *
-                ],
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
