4a726a8a49bffe4fb0ee9b0e8b41463f00405716
KARAN <karankumarshk7@gmail.com>
2026-09-16 21:15:56 +0530

    docs: add missing help strings to setxyturtle and turtlenote blocks (#7801)

--- js/blocks/EnsembleBlocks.js
+++ js/blocks/EnsembleBlocks.js
@@ -401,7 +401,13 @@ function setupEnsembleBlocks(activity) {
             if (_THIS_IS_MUSIC_BLOCKS_) {
                 //.TRANS: set xy position for this mouse
                 super("setxyturtle", _("set mouse"));
-                this.setHelpString();
+                this.setHelpString([
+                    _(
+                        "The Set mouse block places the specified mouse at a specific x and y coordinate."
+                    ),
+                    "documentation",
+                    ""
+                ]);
 
                 this.formBlock({
                     args: 3,
@@ -412,7 +418,13 @@ function setupEnsembleBlocks(activity) {
             } else {
                 //.TRANS: set xy position for this turtle
                 super("setxyturtle", _("set turtle"));
-                this.setHelpString();
+                this.setHelpString([
+                    _(
+                        "The Set turtle block places the specified turtle at a specific x and y coordinate."
+                    ),
+                    "documentation",
+                    ""
+                ]);
 
                 this.formBlock({
                     args: 3,
