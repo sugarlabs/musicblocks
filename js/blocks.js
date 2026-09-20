018a80346bdb3d7762dfb35cfcffe1c82bb9e479
Lavjeet Kumar Rai <railavjeet897@gmail.com>
2026-09-20 17:39:23 +0530

    refactor(blocks): consolidate named block type checks using Set
    - Extract repeated named block types into a Set in renameNameddos
    - Standardize membership checks from .includes() and .indexOf() !== -1 to Set.has()
    - Prevent redundant array allocations across loop iterations
    
    Signed-off-by: Lavjeet Kumar Rai <railavjeet897@gmail.com>

--- js/blocks.js
+++ js/blocks.js
@@ -3712,17 +3712,15 @@ class Blocks {
                 return;
             }
 
+            const namedBlocks = new Set(["nameddo", "namedcalc", "nameddoArg", "namedcalcArg"]);
+
             /** Update the blocks, do->oldName should be do->newName */
             for (const blk in this.blockList) {
                 if (this.blockList[blk].trash) {
                     continue;
                 }
 
-                if (
-                    ["nameddo", "namedcalc", "nameddoArg", "namedcalcArg"].includes(
-                        this.blockList[blk].name
-                    )
-                ) {
+                if (namedBlocks.has(this.blockList[blk].name)) {
                     const targetBlock = this.blockList[blk];
 
                     let activeName = targetBlock.privateData || targetBlock.overrideName;
@@ -3744,8 +3742,7 @@ class Blocks {
             for (let blockId = 0; blockId < actionsPalette.protoList.length; blockId++) {
                 const block = actionsPalette.protoList[blockId];
                 if (
-                    ["nameddo", "namedcalc", "nameddoArg", "namedcalcArg"].indexOf(block.name) !==
-                        -1 /** && block.defaults[0] !== _('action') */ &&
+                    namedBlocks.has(block.name) /** && block.defaults[0] !== _('action') */ &&
                     block.defaults[0] === oldName
                 ) {
                     block.defaults[0] = newName;
