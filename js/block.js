5ed2473440cd02403e110995158eb7161b01c6c3
Adrish Dey <adrishdey2007@gmail.com>
2026-09-25 14:24:47 +0530

    refactor(block): remove dead touch long-press listeners (Related to #8932)
    Signed-off-by: Adrish Dey <adrishdey2007@gmail.com>

--- js/block.js
+++ js/block.js
@@ -3746,31 +3746,6 @@ class Block {
             moved = false;
             that._announced = false;
         });
-        // Touch long-press to open context menu
-        this.container.on("touchstart", () => {
-            that.blocks.mouseDownTime = new Date().getTime();
-            that.blocks.longPressTimeout = setTimeout(() => {
-                that.blocks.activeBlock = thisBlock;
-                that._triggerLongPress = true;
-                that.blocks.triggerLongPress();
-            }, LONGPRESSTIME);
-        });
-
-        this.container.on("touchmove", () => {
-            if (that.blocks.longPressTimeout !== null) {
-                clearTimeout(that.blocks.longPressTimeout);
-                that.blocks.longPressTimeout = null;
-                that.blocks.clearLongPress();
-            }
-        });
-
-        this.container.on("touchend", () => {
-            if (that.blocks.longPressTimeout !== null) {
-                clearTimeout(that.blocks.longPressTimeout);
-                that.blocks.longPressTimeout = null;
-                that.blocks.clearLongPress();
-            }
-        });
     }
 
     /**
